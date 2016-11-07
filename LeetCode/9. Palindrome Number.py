daa7cd5d552b34a75f9bd3de7fd5e3bd4e82a34d
Mingun Pak <kaienmg@gmail.com>
2016-11-06 19:59:03 -0800

    11/06/16 practice

---
+++ LeetCode/9. Palindrome Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return True if str(x) == str(x)[::-1] else False
+
+test = Solution()
+print(test.isPalindrome())
