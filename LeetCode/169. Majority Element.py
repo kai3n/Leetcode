05cbb91394c4f51fedbeabefa4314cce306c5e75
Mingun Pak <kaienmg@gmail.com>
2016-11-13 22:42:56 -0800

    11/13/16 Good

---
+++ LeetCode/169. Majority Element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[len(nums)//2]
+
+test = Solution()
+print(test.majorityElement([3,4,4]))
