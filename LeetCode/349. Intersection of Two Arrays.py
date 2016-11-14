811c3fd2c3feb0f4897a23f1c18d023d091c7458
Mingun Pak <kaienmg@gmail.com>
2016-11-13 20:58:40 -0800

    11/13/16 need to see again

--- LeetCode/349. Intersection of Two Arrays.py
+++ LeetCode/349. Intersection of Two Arrays.py
@@ -1,11 +1,13 @@
-class Solution(object):
-    def intersection(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        return list(set(nums1).intersection(set(nums2)))
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-test = Solution()
-print(test.intersection([1,2,2,1], [2,2]))
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+
+
