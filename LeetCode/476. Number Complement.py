1edc36c396c9cae025a58f08bbce4bdde188479f
Mingun Pak <kaienmg@gmail.com>
2017-01-16 20:03:57 -0800

    01/15/17 easy [try again?] no

---
+++ LeetCode/476. Number Complement.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findComplement(self, num):
+        i = 1
+        while i <= num:
+            i = i << 1
+        return (i - 1) ^ num
+
+test = Solution()
+print(test.findComplement(5))
