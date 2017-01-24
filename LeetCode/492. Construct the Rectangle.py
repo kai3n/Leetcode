03737092fb584dc3d6d9c2d47c42eab2e7768569
Mingun Pak <mingunpa@usc.edu>
2017-01-24 01:11:05 -0800

    01/24/17 my mistake [try again?] no

--- LeetCode/492. Construct the Rectangle.py
+++ LeetCode/492. Construct the Rectangle.py
@@ -1,19 +1,40 @@
-# Memory Limit Exceeded
+# # Memory Limit Exceeded
+# class Solution(object):
+#     def constructRectangle(self, area):
+#         """
+#         :type area: int
+#         :rtype: List[int]
+#         """
+#         l = [area]
+#         for n in range(area//2,0,-1):
+#             if area % n == 0:
+#                 l.append(n)
+#         if len(l) % 2 == 1:
+#             return [l[len(l)//2],l[len(l)//2]]
+#         else:
+#             return [l[(len(l)//2)-1],l[(len(l)//2)]]
+#
+# test = Solution()
+# print(test.constructRectangle(1))
+#
 class Solution(object):
     def constructRectangle(self, area):
         """
         :type area: int
         :rtype: List[int]
         """
-        l = [area]
-        for n in range(area//2,0,-1):
+        import math
+        for n in range(int(math.ceil(math.sqrt(area)))+1,0,-1):
             if area % n == 0:
-                l.append(n)
-        if len(l) % 2 == 1:
-            return [l[len(l)//2],l[len(l)//2]]
-        else:
-            return [l[(len(l)//2)-1],l[(len(l)//2)]]
+                if n**2 == area:
+                    return [n,n]
+                else:
+                    return [max(n, area // n),min(n, area // n)]
+
 
 test = Solution()
-print(test.constructRectangle(1))
+print(test.constructRectangle(134))
+
+
+
 
