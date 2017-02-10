de91650d8451565807b1bbedf1588a0dd8b56390
Mingun Pak <mingunpa@usc.edu>
2017-02-10 11:54:27 -0800

    02/10/17 difficult [try again?] yes

---
+++ LeetCode/388. Longest Absolute File Path.py
@@ -0,0 +1,20 @@
+class Solution(object):
+
+    def lengthLongestPath(self, input):
+        maxlen = 0
+        pathlen = {0: 0}
+        print(input.splitlines())
+        for line in input.splitlines():
+            name = line.lstrip('\t')
+
+            depth = len(line) - len(name)
+            if '.' in name:
+                maxlen = max(maxlen, pathlen[depth] + len(name))
+            else:
+                pathlen[depth + 1] = pathlen[depth] + len(name) + 1
+        return maxlen
+
+
+input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+test = Solution()
+print(test.lengthLongestPath(input))
