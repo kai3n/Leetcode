112cf15ab587038c7fc01271965c897ab98aa29d
Mingun Pak <mingunpa@usc.edu>
2017-02-15 00:35:26 -0800

    02/15/17 difficult [try again?] yes

---
+++ LeetCode/22. Generate Parentheses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+nodeDic = {'a':1,'b':2,'c':3, 'd':1}
+print(max(nodeDic.items(), key=lambda x:x[1])[1])
+#print(a.items())
+#print(a.keys())
+[1,2,3,4,5,6,7,8,9,0]
+print([i[0] for i in nodeDic.items() if i[1] == 1])
---
+++ LeetCode/378. Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        import heapq
+        return list(heapq.merge(*matrix))[k-1]
