252dfc2d71332260542313615e7aae9f92581fe6
Mingun Pak <mingunpa@usc.edu>
2017-02-18 18:39:46 -0800

    02/18/17 easy[try again?] no

---
+++ LeetCode/520. Detect Capital.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        if word == word.upper() or word == word.lower() or (word[0] == word[0].upper() and word[1:] == word[1:].lower()):
+            return True
+        return False
+
+test = Solution()
+print(test.detectCapitalUse('UsdfsS'))
+
