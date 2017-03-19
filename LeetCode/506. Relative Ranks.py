bb2ec6d74acb2492305b114509c3fd792a4d75f9
Mingun Pak <mingunpa@usc.edu>
2017-03-18 23:57:57 -0700

    03/18/17 easy

--- LeetCode/506. Relative Ranks.py
+++ LeetCode/506. Relative Ranks.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        nums = sorted(nums, reverse=True)
+        d = {}
+        for i in range(len(nums)):
+            d[nums[i]] = i
+        sortedNums = sorted(nums, reverse=True)
+
+        count = 1
+        for j in sortedNums:
+            if count == 1:
+                nums[d[j]] = "Gold Medal"
+            elif count == 2:
+                nums[d[j]] = "Silver Medal"
+            elif count == 3:
+                nums[d[j]] = "Bronze Medal"
+            else:
+                nums[d[j]] = str(count)
+            count += 1
+        return nums
+
+test = Solution()
+print(test.findRelativeRanks([5,4,3,2,1]))
