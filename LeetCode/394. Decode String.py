6b6c047ac70d0ec0d0f11829634cb7fe77f49622
Mingun Pak <mingunpa@usc.edu>
2017-09-05 14:46:36 -0700

    Google Question: add other people's solution

--- LeetCode/394. Decode String.py
+++ LeetCode/394. Decode String.py
@@ -45,3 +45,24 @@ test = Solution()
 print(test.decodeString('3[a]2[bc]'))
 print(test.decodeString('3[a2[c]]'))
 print(test.decodeString('2[abc]3[cd]ef'))
+
+
+# other people's solution
+
+class Solution(object):
+    def decodeString(self, s):
+        stack = []
+        stack.append(["", 1])
+        num = ""
+        for ch in s:
+            if ch.isdigit():
+              num += ch
+            elif ch == '[':
+                stack.append(["", int(num)])
+                num = ""
+            elif ch == ']':
+                st, k = stack.pop()
+                stack[-1][0] += st*k
+            else:
+                stack[-1][0] += ch
+        return stack[0][0]
