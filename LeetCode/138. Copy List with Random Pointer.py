fa190df8088ea7dad08d24341d466b6d1ef30819
Mingun Pak <mingunpa@usc.edu>
2017-09-18 15:48:53 -0700

    good solution

---
+++ LeetCode/138. Copy List with Random Pointer.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list with a random pointer.
+class RandomListNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+class Solution:
+    def copyRandomList(self, head):
+        dic = dict()
+        m = n = head
+        while m:
+            dic[m] = RandomListNode(m.label)
+            m = m.next
+        while n:
+            dic[n].next = dic.get(n.next)
+            dic[n].random = dic.get(n.random)
+            n = n.next
+        return dic.get(head)
