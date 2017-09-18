c671b95d963cb205086ae7e492e8d03f26c36fa8
Mingun Pak <mingunpa@usc.edu>
2017-09-18 16:16:27 -0700

    with some trick

--- LeetCode/22. Generate Parentheses.py
+++ LeetCode/22. Generate Parentheses.py
@@ -1,7 +1,25 @@
-def generateParenthesis(self, n):
-    def generate(p, left, right, parens=[]):
-        if left:         generate(p + '(', left-1, right)
-        if right > left: generate(p + ')', left, right-1)
-        if not right:    parens += p,
-        return parens
-    return generate('', n, n)
+class Solution(object):
+
+    def generateParenthesis(self, n):
+        def generate(p, left, right, parens):
+            if left:
+                generate(p + '(', left-1, right,parens)
+            if right > left:
+                generate(p + ')', left, right-1,parens)
+            if not right:
+                parens += p,
+            return parens
+        return generate('', n, n, [])
+
+    def generateParenthesis2(self, n):
+        def generate(p, left, right, parens=[]):
+            if left:         generate(p + '(', left - 1, right)
+            if right > left: generate(p + ')', left, right - 1)
+            if not right:    parens += p,
+            return parens
+
+        return generate('', n, n)
+
+test = Solution()
+print(test.generateParenthesis(4))
+
