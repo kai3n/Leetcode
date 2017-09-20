1bf7366dc518bea4b57c71a34a4d5af345983deb
Mingun Pak <mingunpa@usc.edu>
2017-09-20 15:37:40 -0700

    Sudoku

---
+++ LeetCode/36. Valid Sudoku.py
@@ -0,0 +1,16 @@
+def isValidSudoku(self, board):
+    """
+    :type board: List[List[str]]
+    :rtype: bool
+    """
+    big = set()
+    for i in range(0,9):
+        for j in range(0,9):
+            if board[i][j]!='.':
+                cur = board[i][j]
+                if (i,cur) in big or (cur,j) in big or (i/3,j/3,cur) in big:
+                    return False
+                big.add((i,cur))
+                big.add((cur,j))
+                big.add((i/3,j/3,cur))
+    return True
