ab06358a1ceb01d57976b6afd2dac7cf01120cc7
Mingun Pak <mingunpa@usc.edu>
2017-09-21 15:30:42 -0700

    try again

--- LeetCode/287. Find the Duplicate Number.py
+++ LeetCode/287. Find the Duplicate Number.py
@@ -17,3 +17,25 @@ class Solution(object):
             slow = nums[slow]
 
         return slow
+
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        low = 1
+        high = len(nums) - 1
+
+        while low < high:
+            mid = low + (high - low) / 2
+            count = 0
+            for i in nums:
+                if i <= mid:
+                    count += 1
+            if count <= mid:
+                low = mid + 1
+            else:
+                high = mid
+        return low
