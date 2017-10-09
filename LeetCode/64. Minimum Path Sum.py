40024bde6de7200b4e8d05a9e04ee9a027629a9e
Mingun Pak <mingunpa@usc.edu>
2017-10-09 13:56:10 -0700

    easy dp

--- LeetCode/64. Minimum Path Sum.py
+++ LeetCode/64. Minimum Path Sum.py
@@ -4,8 +4,13 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        for i in range(grid):
-            for j in range(grid[0]):
-                if 0 <= i - 1< len(grid) and 0 <= j - 1< len(grid[0]):
-                    grid[i][j] = min(grid[i][j] + grid[i-1][j], grid[i][j] + grid[i][j-1])
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if 0 <= i - 1 < len(grid) and 0 <= j - 1 < len(grid[0]):
+                    grid[i][j] = min(grid[i][j] + grid[i - 1][j], grid[i][j] + grid[i][j - 1])
+                elif 0 <= i - 1 < len(grid):
+                    grid[i][j] += grid[i - 1][j]
+                elif 0 <= j - 1 < len(grid[0]):
+                    grid[i][j] += grid[i][j - 1]
         return grid[-1][-1]
