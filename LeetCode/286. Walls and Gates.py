20fd4050ed85bc091fe4ecbd5e7fb7d981373c9f
Mingun Pak <mingunpa@usc.edu>
2017-10-11 20:19:56 -0700

    basic bfs

--- LeetCode/286. Walls and Gates.py
+++ LeetCode/286. Walls and Gates.py
@@ -1,12 +1,12 @@
-def bfs(rooms, y, x):
-    queue = [[y, x]]
-    while queue:
-        n_y, n_x = queue.pop()
-        for i in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            if 0 <= n_y+i[0] < len(rooms) and 0 <= n_x+i[1] < len(rooms[0]):
-                if rooms[n_y + i[0]][n_x + i[1]] > rooms[n_y][n_x]+1:
-                    rooms[n_y + i[0]][n_x + i[1]] = rooms[n_y][n_x]+1
-                    queue.append([n_y + i[0], n_x + i[1]])
+# def bfs(rooms, y, x):
+#     queue = [[y, x]]
+#     while queue:
+#         n_y, n_x = queue.pop()
+#         for i in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+#             if 0 <= n_y+i[0] < len(rooms) and 0 <= n_x+i[1] < len(rooms[0]):
+#                 if rooms[n_y + i[0]][n_x + i[1]] > rooms[n_y][n_x]+1:
+#                     rooms[n_y + i[0]][n_x + i[1]] = rooms[n_y][n_x]+1
+#                     queue.append([n_y + i[0], n_x + i[1]])
 
 
 class Solution(object):
@@ -16,10 +16,19 @@ class Solution(object):
         :rtype: void Do not return anything, modify rooms in-place instead.
         """
 
+        queue = []
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
-                    bfs(rooms, i, j)
+                    queue.append([i, j])
+        while queue:
+            n_y, n_x = queue.pop()
+            for i in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                if 0 <= n_y + i[0] < len(rooms) and 0 <= n_x + i[1] < len(rooms[0]):
+                    if rooms[n_y + i[0]][n_x + i[1]] > rooms[n_y][n_x] + 1:
+                        rooms[n_y + i[0]][n_x + i[1]] = rooms[n_y][n_x] + 1
+                        queue.append([n_y + i[0], n_x + i[1]])
+
         return rooms
 
 rooms = [[2147483647,-1,0,2147483647],
