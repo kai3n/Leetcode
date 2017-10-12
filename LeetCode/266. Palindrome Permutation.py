8c4a7e58c2ee36cd8482931a4fbb574c2016dd63
Mingun Pak <mingunpa@usc.edu>
2017-10-12 13:39:43 -0700

    easy

---
+++ LeetCode/266. Palindrome Permutation.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def canPermutePalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        from collections import defaultdict
+
+        d = defaultdict(int)
+        for i in s:
+            d[i] += 1
+        c = 0
+        for k, v in d.items():
+            if v % 2:
+                c += 1
+        if c > 1:
+            return False
+        return True
+
+
+
+
+a = "code"  # False
+b = "aab"  # True
+c = "carerac" # True
+
+test = Solution()
+print(test.canPermutePalindrome(a))
+print(test.canPermutePalindrome(b))
+print(test.canPermutePalindrome(c))
+
+
