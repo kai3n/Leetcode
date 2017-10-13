be7e460416cca1561e005234b9ac917f73f5681e
Mingun Pak <mingunpa@usc.edu>
2017-10-13 13:35:31 -0700

    Good Solution

--- LeetCode/236. Lowest Common Ancestor of a Binary Tree.py
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree.py
@@ -5,40 +5,39 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-def helper(root, v):
-    if root:
-        if root.val == v:
-            return True
-        return helper(root.left, v) or helper(root.right, v)
-    return False
+
 
 
 
 class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
+
+    def helper(self, root, target):
         if root:
-            if root.val == p and (helper(root.left, q) or helper(root.right, q)):
+            if root == target:
+                return True
+            return self.helper(root.left, target) or self.helper(root.right, target)
+        return False
+
+    def lowestCommonAncestor(self, root, p, q):
+
+        while root:
+            if self.helper(p, q):
+                return p
+            if self.helper(q, p):
+                return q
+            if self.helper(root.left, p) and self.helper(root.left, q):
+                root = root.left
+            if self.helper(root.right, p) and self.helper(root.right, q):
+                root = root.right
+            else:
                 return root
-            elif root.val == q and (helper(root.left, p) or helper(root.right, p)):
-                return root
-            elif helper(root.left, p) and helper(root.right, q):
-                return root
-            elif helper(root.left, q) and helper(root.right, p):
-                return root
-            l = self.lowestCommonAncestor(root.left, p, q)
-            if l != -1:
-                return l
-            r = self.lowestCommonAncestor(root.right, p, q)
-            if r != -1:
-                return r
-        return -1
 
 node = TreeNode(2)
 node.right = TreeNode(1)
 # node.right = TreeNode(4)
 # node.right.left = TreeNode(6)
 
-print(helper(node, 7))
+
 
 test = Solution()
-print(test.lowestCommonAncestor(node, 1, 2).val)
+print(test.lowestCommonAncestor(node, node, node.right))
