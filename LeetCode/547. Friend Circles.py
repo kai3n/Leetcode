10d826977c9d1c39f1b6217487bd18d769c7a37a
Mingun Pak <mingunpa@usc.edu>
2017-10-19 12:08:38 -0700

    dfs

---
+++ LeetCode/547. Friend Circles.py
@@ -0,0 +1,37 @@
+def dfs(board, visited_list, node):
+    stack = [node]
+    visited_list[node] = 1
+    while stack:
+        n = stack.pop()
+        for i in range(len(board[n])):
+            if i != n and board[n][i] == 1 and visited_list[i] == 0:
+                visited_list[i] = 1
+                stack.append(i)
+
+class Solution(object):
+    def findCircleNum(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        if len(M) == 1:
+            return 1
+        visited_list = [0] * len(M)
+        count = 0
+        for i in range(len(visited_list)):
+            if visited_list[i] == 1:
+                continue
+            dfs(M, visited_list, i)
+            count += 1
+        return count
+
+
+
+M = [[1,1,0],
+     [1,1,0],
+     [0,0,1]]
+
+
+
+test = Solution()
+print(test.findCircleNum(M))
--- LeetCode/647. Palindromic Substrings.py
+++ LeetCode/647. Palindromic Substrings.py
@@ -1,28 +1,57 @@
+# class Solution(object):
+#     def countSubstrings(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         # Brute Force Algorithm
+#         count = 0
+#         for i in range(len(s)):
+#             for j in range(len(s)-i):
+#                 if s[j:j+i+1] == s[j:j+i+1][::-1]:
+#                     count += 1
+#         return count
+#
+# test = Solution()
+# print(test.countSubstrings("abcgqadbbggrfeeetjioeeqdk"))
+#
+# def countSubstrings(self, S):
+#     N = len(S)
+#     ans = 0
+#     for center in range(2*N - 1):
+#         left = center / 2
+#         right = left + center % 2
+#         while left >= 0 and right < N and S[left] == S[right]:
+#             ans += 1
+#             left -= 1
+#             right += 1
+#     return ans
+#
 class Solution(object):
     def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Brute Force Algorithm
+
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
         count = 0
+        k = 0
         for i in range(len(s)):
-            for j in range(len(s)-i):
-                if s[j:j+i+1] == s[j:j+i+1][::-1]:
+            for j in range(len(s) - k):
+                if k == 0:
+                    dp[j][j + k] = True
+                elif k == 1 and s[j] == s[j + k]:
+                    dp[j][j + k] = True
+                elif s[j] != s[j + k]:
+                    continue
+                else:  # same character
+                    if 0 <= j + 1 < len(s) and 0 <= j + k - 1 < len(s):
+                        dp[j][j + k] = dp[j + 1][j + k - 1]
+            k += 1
+
+        for i in range(len(dp)):
+            for j in range(len(dp[0])):
+                if dp[i][j] == True:
                     count += 1
+
         return count
 
 test = Solution()
-print(test.countSubstrings("abcgqadbbggrfeeetjioeeqdk"))
-
-def countSubstrings(self, S):
-    N = len(S)
-    ans = 0
-    for center in range(2*N - 1):
-        left = center / 2
-        right = left + center % 2
-        while left >= 0 and right < N and S[left] == S[right]:
-            ans += 1
-            left -= 1
-            right += 1
-    return ans
+print(test.countSubstrings('abaabc'))
