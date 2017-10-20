82e610441ef56ec4d97c1354ea88d7f6911bbbdf
Mingun Pak <mingunpa@usc.edu>
2017-10-20 16:53:44 -0700

    clever

---
+++ LeetCode/95. Unique Binary Search Trees II.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def generateTrees(self, n):
+        if n == 0:
+            return []
+        return self.dfs(1, n+1)
+    def dfs(self, start, end):
+        if start == end:
+            return None
+        result = []
+        for i in range(start, end):
+            for l in self.dfs(start, i) or [None]:
+                for r in self.dfs(i+1, end) or [None]:
+                    node = TreeNode(i)
+                    node.left, node.right = l, r
+                    result.append(node)
+        return result
