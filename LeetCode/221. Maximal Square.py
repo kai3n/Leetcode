425492503081a028ddb7abf6926aa30c51b993db
Mingun Pak <mingunpa@usc.edu>
2017-10-20 14:46:09 -0700

    clever

--- LeetCode/221. Maximal Square.py
+++ LeetCode/221. Maximal Square.py
@@ -52,6 +52,19 @@ class Solution(object):
             print()
         return maxSquare ** 2
 
+class Solution(object):
+    def maximalSquare(self, matrix):
+        if len(matrix) == 0:
+            return 0
+        res = 0
+        dp = [[0 for j in range(len(matrix[0])+1)] for i in range(len(matrix)+1)]
+        for i in range(1, len(matrix)+1):
+            for j in range(1, len(matrix[0])+1):
+                if matrix[i-1][j-1] == '1':
+                    dp[i][j] = min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j]) + 1
+                    res = max(res, dp[i][j])
+        return res**2
+
 matrix = [[1,0,1,0,0],
           [1,0,1,1,1],
           [1,1,1,1,1],
