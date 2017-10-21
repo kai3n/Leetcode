915e5027af56f6275bc0bd0d8cd681442b994d82
Mingun Pak <mingunpa@usc.edu>
2017-10-20 18:53:46 -0700

    brute force solution

---
+++ LeetCode/300. Longest Increasing Subsequence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        res = 1
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    res = max(res, dp[i])
+        return res
