8b8dc7f96b262426c9ca86f79bf5165bbcca2099
Mingun Pak <mingunpa@usc.edu>
2017-10-23 16:45:34 -0700

    good

---
+++ LeetCode/501. Find Mode in Binary Search Tree.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findMode(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        from collections import Counter
+        count = Counter()
+
+        def dfs(node):
+            if node:
+                count[node.val] += 1
+                dfs(node.left)
+                dfs(node.right)
+
+        dfs(root)
+        max_count = max(count.itervalues())
+        return [k for k, v in count.iteritems() if v == max_count]
