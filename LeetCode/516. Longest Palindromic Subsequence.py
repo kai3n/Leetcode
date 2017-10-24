78dcabe63a6ffd77a99ebafe7072bc45268fe1b5
Mingun Pak <mingunpa@usc.edu>
2017-10-24 14:24:20 -0700

    must see again

--- LeetCode/516. Longest Palindromic Subsequence.py
+++ LeetCode/516. Longest Palindromic Subsequence.py
@@ -1,3 +1,9 @@
+def print_2darray(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            print(arr[i][j], ' ', end='')
+        print()
+    print()
 class Solution(object):
     def longestPalindromeSubseq(self, s):
         """
@@ -6,32 +12,39 @@ class Solution(object):
         """
         dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
 
-        for i in range(len(s), -1, -1):
+        for i in range(len(s)-1, -1, -1):
+            dp[i][i] = 1
             for j in range(i+1, len(s)):
                 if s[i] == s[j]:
                     dp[i][j] = dp[i+1][j-1] + 2
                 else:
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+            print_2darray(dp)
         return dp[0][-1]
 
+s = "bbbab"
+#s = "bb"
+test = Solution()
+test.longestPalindromeSubseq(s)
 # O(n) space
-class Solution(object):
-    def longestPalindromeSubseq(self, s):
-        if s == s[::-1]:
-            return len(s)
 
-        n = len(s)
-        dp = [0 for j in range(n)]
-        dp[n - 1] = 1
-
-        for i in range(n - 1, -1, -1):
-            newdp = dp[:]
-            newdp[i] = 1
-            for j in xrange(i + 1, n):
-                if s[i] == s[j]:
-                    newdp[j] = 2 + dp[j - 1]
-                else:
-                    newdp[j] = max(dp[j], newdp[j - 1])
-            dp = newdp
-
-        return dp[n - 1]
+# class Solution(object):
+#     def longestPalindromeSubseq(self, s):
+#         if s == s[::-1]:
+#             return len(s)
+#
+#         n = len(s)
+#         dp = [0 for j in range(n)]
+#         dp[n - 1] = 1
+#
+#         for i in range(n - 1, -1, -1):
+#             newdp = dp[:]
+#             newdp[i] = 1
+#             for j in xrange(i + 1, n):
+#                 if s[i] == s[j]:
+#                     newdp[j] = 2 + dp[j - 1]
+#                 else:
+#                     newdp[j] = max(dp[j], newdp[j - 1])
+#             dp = newdp
+#
+#         return dp[n - 1]
