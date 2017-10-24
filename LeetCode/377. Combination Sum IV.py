4190597541ef75c7ec7c62bdf35f5d02822917de
Mingun Pak <mingunpa@usc.edu>
2017-10-23 17:20:18 -0700

    good

---
+++ LeetCode/377. Combination Sum IV.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        res = [0] * (target + 1)
+        res[0] = 1
+        for i in range(1, target+1):
+            for x in nums:
+                if i >= x:
+                    res[i] += res[i-x]
+        return res[target]
