159668f6e7ab8a941468f0ddfb99d74800efc353
Mingun Pak <mingunpa@usc.edu>
2017-10-31 22:29:30 -0700

    annoying!

---
+++ LeetCode/57. Insert Interval.py
@@ -0,0 +1,21 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        s, e = newInterval.start, newInterval.end
+        left, right = [], []
+        for i in intervals:
+            if i.end < s:
+                left += i,
+            elif i.start > e:
+                right += i,
+            else:
+                s = min(s, i.start)
+                e = max(e, i.end)
+        return left + [Interval(s, e)] + right
+
+
