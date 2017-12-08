d2554a9c72087a043fea35364d76d38e083cacec
Mingun Pak <mingunpa@usc.edu>
2017-12-08 14:10:43 -0800

    dirty code

---
+++ LeetCode/737. Sentence Similarity II.py
@@ -0,0 +1,41 @@
+class Solution:
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2):
+            return False
+
+        from collections import defaultdict
+
+        d = defaultdict(set)
+        for pair in pairs:
+            d[pair[0]].add(pair[1])
+            d[pair[1]].add(pair[0])
+
+        g = {}
+
+        def dfs(n, r):
+            for nn in d[n]:
+                if nn not in g.keys():
+                    g[nn] = r
+                    dfs(nn, r)
+
+        for n in d.keys():
+            if n not in g.keys():
+                g[n] = n
+                dfs(n, n)
+
+        print(g)
+        for i, j in zip(words1, words2):
+            if i == j:
+                continue
+            else:
+                if g.get(i, None) != g.get(j, None):
+                    return False
+                else:
+                    continue
+        return True
