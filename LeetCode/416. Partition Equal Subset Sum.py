c49f8a4c392cbaebaae5996e09c54712487f3fa4
Mingun Pak <mingunpa@usc.edu>
2017-12-10 18:43:57 -0800

    DFS with cache

--- LeetCode/416. Partition Equal Subset Sum.py
+++ LeetCode/416. Partition Equal Subset Sum.py
@@ -1,19 +1,18 @@
-class Solution:
+class Solution(object):
     def canPartition(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-
+        cache = set()
         def helper(start, target):
+            if (start, target) in cache:
+                return False
             if target < 0:
                 return
             elif target == 0:
                 return True
             else:
                 for i in range(start, len(nums)):
-                    if helper(i + 1, target - nums[i]):
+                    if helper(i+1, target-nums[i]):
                         return True
+                cache.add((start, target))
                 return False
 
-        return False if sum(nums) % 2 else helper(0, sum(nums) / 2)
+        return False if sum(nums)%2 else helper(0, sum(nums)/2)
