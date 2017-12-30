4918e60ca272deaa57ed65b6563b9ddf402941a7
Mingun Pak <mingunpa@usc.edu>
2017-12-30 15:48:08 +0900

    easy

---
+++ LeetCode/748. Largest Number At Least Twice of Others.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_num = max(nums)
+        max_idx = 0
+        for i, num in enumerate(nums):
+            if num == max_num:
+                max_idx = i
+                continue
+            if num * 2 > max_num:
+                return -1
+        return max_idx
