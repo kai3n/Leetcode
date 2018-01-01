6293dce6c7bda8581d988da2096b91d9e255f850
Mingun Pak <mingunpa@usc.edu>
2018-01-01 18:44:50 +0900

    added dfs solution

--- LeetCode/78. Subsets.py
+++ LeetCode/78. Subsets.py
@@ -18,3 +18,15 @@ class Solution(object):
             res.append(subset)
         return res
 
+
+# DFS recursively
+def subsets1(self, nums):
+    res = []
+    self.dfs(sorted(nums), 0, [], res)
+    return res
+
+
+def dfs(self, nums, index, path, res):
+    res.append(path)
+    for i in range(index, len(nums)):
+        self.dfs(nums, i + 1, path + [nums[i]], res)
