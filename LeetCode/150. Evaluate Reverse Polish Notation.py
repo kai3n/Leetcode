e5331873a8e7b3bc32ee5daae4529ea4b01dcac2
Mingun Pak <mingunpa@usc.edu>
2018-01-04 17:51:02 +0900

    DB Question

--- LeetCode/150. Evaluate Reverse Polish Notation.py
+++ LeetCode/150. Evaluate Reverse Polish Notation.py
@@ -13,9 +13,6 @@ class Solution:
                 elif t == "*":
                     stack.append(l*r)
                 else:
-                    # here take care of the case like "1/-22",
-                    # in Python 2.x, it returns -1, while in
-                    # Leetcode it should return 0
                     if l*r < 0 and l % r != 0:
                         stack.append(l//r+1)
                     else:
--- LeetCode/744. Find Smallest Letter Greater Than Target.py
+++ LeetCode/744. Find Smallest Letter Greater Than Target.py
@@ -9,3 +9,18 @@ class Solution:
             if letter > target:
                 return letter
         return letters[0]
+
+arr = [1,3,5,7,9,11]
+target = 8
+first = 0
+last = len(arr) - 1
+
+while first <= last:
+    mid = (first + last) // 2
+    if arr[mid] == target:
+        break
+    elif arr[mid] > target:
+        last = mid - 1
+    else:
+        first = mid + 1
+print(first)
