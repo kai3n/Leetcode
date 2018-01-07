d1984c554b1c17eca6e0f5bf39e649705a8289da
Mingun Pak <mingunpa@usc.edu>
2018-01-07 00:24:52 -1000

    added another solution

--- LeetCode/245. Shortest Word Distance III.py
+++ LeetCode/245. Shortest Word Distance III.py
@@ -28,3 +28,24 @@ class Solution:
                     res = min(res, abs(a[j] - a[i]))
         return res
 
+# another solution
+def shortestWordDistance(self, words, word1, word2):
+    """
+    :type words: List[str]
+    :type word1: str
+    :type word2: str
+    :rtype: int
+    """
+    p1, p2 = -1, -1
+    dis = float("inf")
+    flip = True
+    for i,w in enumerate(words):
+        if w==word1 and flip:
+            if word1==word2: flip = False
+            p1 = i
+        elif w==word2:
+            if word1==word2: flip = True
+            p2 = i
+        if p1!=-1 and p2!=-1:
+            dis = min(dis, abs(p1-p2))
+    return dis
