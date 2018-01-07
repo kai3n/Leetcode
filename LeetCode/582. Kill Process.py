9f44ed9e24026bb4abebf72e847abd4f468f97f9
Mingun Pak <mingunpa@usc.edu>
2018-01-06 23:42:59 -1000

    Dic

--- LeetCode/582. Kill Process.py
+++ LeetCode/582. Kill Process.py
@@ -1,4 +1,25 @@
-# TIme Limit Solution
+# # Time Limit Solution
+# class Solution:
+#     def killProcess(self, pid, ppid, kill):
+#         """
+#         :type pid: List[int]
+#         :type ppid: List[int]
+#         :type kill: int
+#         :rtype: List[int]
+#         """
+#
+#         queue = [kill]
+#         res = []
+#         while queue:
+#             node = queue.pop(0)
+#             res.append(node)
+#             for i in range(len(pid)):
+#                 if ppid[i] == node:
+#                     queue.append(pid[i])
+#         return res
+
+
+# AC Solution
 class Solution:
     def killProcess(self, pid, ppid, kill):
         """
@@ -7,13 +28,27 @@ class Solution:
         :type kill: int
         :rtype: List[int]
         """
-
-        queue = [kill]
+        d = {}
         res = []
-        while queue:
-            node = queue.pop(0)
-            res.append(node)
-            for i in range(len(pid)):
-                if ppid[i] == node:
-                    queue.append(pid[i])
+        q = [kill]
+
+        # Preprocessing
+        for i in range(len(ppid)):
+            if d.get(ppid[i], None) is None:
+                d[ppid[i]] = [pid[i]]
+            else:
+                d[ppid[i]].append(pid[i])
+
+        while q:
+            n = q.pop(0)
+            res.append(n)
+            if d.get(n, None) is not None:
+                q.extend(d[n])
         return res
+
+pid =  [1, 3, 10, 5]
+ppid = [3, 0, 5, 3]
+kill = 5
+
+test = Solution()
+print(test.killProcess(pid, ppid, kill))
