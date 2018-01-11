3ca53e13982256f3ef309eb72c2d728ba5640ade
Mingun Pak <mingunpa@usc.edu>
2018-01-11 09:15:21 -0800

    added the right solution

--- LeetCode/134. Gas Station.py
+++ LeetCode/134. Gas Station.py
@@ -22,3 +22,20 @@ class Solution:
         return -1
 
 
+# Awesome Solution
+
+"""
+If car starts at A and can not reach B. Any station between A and B
+can not reach B.(B is the first station that A can not reach.)
+If the total number of gas is bigger than the total number of cost. There must be a solution.
+"""
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        gas_left = gas_needed = start = 0
+        for i, (g, c) in enumerate(zip(gas, cost)):
+            gas_left += g - c
+            if gas_left < 0:
+                gas_needed -= gas_left
+                start = i + 1
+                gas_left = 0
+        return start if gas_left >= gas_needed else -1
