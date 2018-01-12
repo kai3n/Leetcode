2a2075e29a4f23e922ea0749d7fae30e88780a04
Mingun Pak <mingunpa@usc.edu>
2018-01-12 11:39:58 -0800

    BFS

---
+++ LeetCode/199. Binary Tree Right Side View.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        lvl = 0
+        queue = [[root, lvl]]
+        prev = root
+        res = []
+
+        while queue:
+            node, level = queue.pop(0)
+
+            if level != lvl:
+                res.append(prev.val)
+                lvl = level
+            if root:
+                if node.left:
+                    queue.append([node.left, level + 1])
+                if node.right:
+                    queue.append([node.right, level + 1])
+            prev = node
+        if node:
+            res.append(node.val)
+        return res
+
