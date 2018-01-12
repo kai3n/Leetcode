5b55a91e3147de5e117f66036f35cdec32e791b4
Mingun Pak <mingunpa@usc.edu>
2018-01-12 14:24:20 -0800

    Simple Solution

--- LeetCode/119. Pascal's Triangle II.py
+++ LeetCode/119. Pascal's Triangle II.py
@@ -16,3 +16,15 @@ class Solution(object):
                 l += 1
         num.append(1)
         return num
+
+# Simple Solution
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
