7d45515be9e2a72dad3d8908f0c58d6e3d8e9d24
Mingun Pak <mingunpa@usc.edu>
2018-01-11 20:13:57 -0800

    Trie and DFS

--- LeetCode/212. Word Search II.py
+++ LeetCode/212. Word Search II.py
@@ -28,3 +28,44 @@ class Solution:
                 visited[i][j] = False
 
         return res
+
+# Trie and DFS
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.flag = False
+
+class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+        self.result = []
+
+    def insert(self, word):
+        node = self.root
+        for letter in word:
+            node = node.children[letter]
+        node.flag = True
+
+    def findWords(self, board, words):
+        for w in words:
+            self.insert(w)
+        for j in range(len(board)):
+            for i in range(len(board[0])):
+                self.dfs(self.root, board, j, i)
+        return self.result
+
+    def dfs(self, node, board, j, i, word=''):
+        if node.flag:
+            self.result.append(word)
+            node.flag = False
+        if 0 <= j < len(board) and 0 <= i < len(board[0]):
+            char = board[j][i]
+            child = node.children.get(char)
+            if child is not None:
+                word += char
+                board[j][i] = None
+                self.dfs(child, board, j + 1, i, word)
+                self.dfs(child, board, j - 1, i, word)
+                self.dfs(child, board, j, i + 1, word)
+                self.dfs(child, board, j, i - 1, word)
+                board[j][i] = char
