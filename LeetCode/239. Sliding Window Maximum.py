2addce1891fcd778728dd678e3107611457063b3
Mingun Pak <mingunpa@usc.edu>
2018-01-11 20:46:37 -0800

    added O(n) Solution

--- LeetCode/239. Sliding Window Maximum.py
+++ LeetCode/239. Sliding Window Maximum.py
@@ -12,3 +12,23 @@ class Solution:
         for i in range(len(nums) - k + 1):
             res.append(max(nums[i:i + k]))
         return res
+
+# So difficult to come up with this idea...
+def maxSlidingWindow(nums, k):
+    from collections import deque
+    d = deque()
+    out = []
+    for i, n in enumerate(nums):
+        while d and nums[d[-1]] < n:
+            d.pop()
+        d += i,
+        if d[0] == i - k:
+            d.popleft()
+        if i >= k - 1:
+            out += nums[d[0]],
+    return out
+
+nums = [1,3,-1,-3,5,3,6,7]
+k = 3
+
+print(maxSlidingWindow(nums, k))
