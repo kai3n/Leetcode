4f98e1b2c2f10c1f547c8ed18e644069adbd6862
Mingun Pak <mingunpa@usc.edu>
2018-01-11 17:58:42 -0800

    added recursive and iterative ways

--- LeetCode/230. Kth Smallest Element in a BST.py
+++ LeetCode/230. Kth Smallest Element in a BST.py
@@ -23,3 +23,34 @@ class Solution:
 
         traversal(root)
         return res[k - 1]
+
+# Recursive
+
+def kthSmallest(self, root, k):
+    self.k = k
+    self.res = None
+    self.helper(root)
+    return self.res
+
+def helper(self, node):
+    if not node:
+        return
+    self.helper(node.left)
+    self.k -= 1
+    if self.k == 0:
+        self.res = node.val
+        return
+    self.helper(node.right)
+
+# Iterative
+def kthSmallest(root, k):
+    stack = []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.val
+        root = root.right
