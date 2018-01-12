bda5ff2cd85fda32fcda9516f93f2c5341e097a6
Mingun Pak <mingunpa@usc.edu>
2018-01-11 17:36:46 -0800

    try again

--- LeetCode/204. Count Primes.py
+++ LeetCode/204. Count Primes.py
@@ -6,6 +6,6 @@ class Solution:
         res[0] = res[1] = False
         for i in range(2, n):
             if res[i] == True:
-                for j in range(2, (n - 1) / i + 1):
+                for j in range(2, (n - 1) // i + 1):
                     res[i * j] = False
         return sum(res)
