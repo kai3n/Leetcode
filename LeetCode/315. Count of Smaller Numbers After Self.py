cd3902dcdfeccafc27a3b0c863dcc5f1f0f9afa5
Mingun Pak <mingunpa@usc.edu>
2018-01-12 19:28:36 -0800

    bisect

--- LeetCode/315. Count of Smaller Numbers After Self.py
+++ LeetCode/315. Count of Smaller Numbers After Self.py
@@ -1,8 +1,12 @@
 import bisect
-def countSmaller(self, nums):
+
+def countSmaller(nums):
     counts, sortednums = [], []
     for i in range(len(nums)-1,-1, -1):
         position = bisect.bisect_left(sortednums, nums[i])
         sortednums.insert(position, nums[i])
         counts.append(position)
     return counts[::-1]
+
+nums = [5, 2, 6, 1]
+print(countSmaller(nums))
