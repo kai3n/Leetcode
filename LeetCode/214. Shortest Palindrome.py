43c02e9c86d31808a9294ab7d6e66986dcfd3a08
Mingun Pak <mingunpa@usc.edu>
2018-01-13 10:19:56 -0800

    KMP Algorithm

--- LeetCode/214. Shortest Palindrome.py
+++ LeetCode/214. Shortest Palindrome.py
@@ -59,4 +59,16 @@ s = "aacecaaa"
 test = Solution()
 print(test.shortestPalindrome(s))
 
+# KMP Algorithm
+def shortestPalindrome(s):
+    A=s+"*"+s[::-1]
+    cont=[0]
+    for i in range(1,len(A)):
+        index=cont[i-1]
+        while(index>0 and A[index]!=A[i]):
+            index=cont[index-1]
+        cont.append(index+(1 if A[index]==A[i] else 0))
+    return s[cont[-1]:][::-1]+s
 
+s = "aacecaaa"
+print(shortestPalindrome(s))
