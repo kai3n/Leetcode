a51c04075fc3d5f9216e9d3c1d211a2d3bf079c3
Mingun Pak <mingunpa@usc.edu>
2018-01-17 13:55:58 -0800

    Added Accepted Solution

--- LeetCode/336. Palindrome Pairs.py
+++ LeetCode/336. Palindrome Pairs.py
@@ -14,3 +14,25 @@ class Solution(object):
                 if i != j and is_palindrome(words[i], words[j]):
                     res.append([i, j])
         return res
+
+    # Accepted Solution
+    def palindromePairs(self, words):
+        def is_palindrome(check):
+            return check == check[::-1]
+
+        words = {word: i for i, word in enumerate(words)}
+        valid_pals = []
+        for word, k in words.iteritems():
+            n = len(word)
+            for j in range(n+1):
+                pref = word[:j]
+                suf = word[j:]
+                if is_palindrome(pref):
+                    back = suf[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([words[back],  k])
+                if j != n and is_palindrome(suf):
+                    back = pref[::-1]
+                    if back != word and back in words:
+                        valid_pals.append([k, words[back]])
+        return valid_pals
