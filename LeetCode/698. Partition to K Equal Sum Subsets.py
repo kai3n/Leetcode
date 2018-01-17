bb14fd42dce1e595e796ea97bf832492e895901c
Mingun Pak <mingunpa@usc.edu>
2018-01-17 11:25:23 -0800

    the most optimized solution

--- LeetCode/698. Partition to K Equal Sum Subsets.py
+++ LeetCode/698. Partition to K Equal Sum Subsets.py
@@ -56,3 +56,36 @@ class Solution(object):
             return False
 
         return dfs(k,0,0,0)
+
+# most optimized solution
+class Solution(object):
+    def canPartitionKSubsets(self, nums, k):
+        if k == 1: return True
+
+        self.n = len(nums)
+        if k > self.n: return False
+
+        total = sum(nums)
+        if total % k: return False
+
+        self.target = total / k
+        visit = [0] * self.n
+
+        if self.target < max(nums):
+            return False
+
+        nums.sort(reverse=True)
+
+        def dfs(k, ind, sum, cnt):
+            if k == 1: return True
+            if sum == self.target and cnt > 0:
+                return dfs(k - 1, 0, 0, 0)
+            for i in range(ind, self.n):
+                if not visit[i] and sum + nums[i] <= self.target:
+                    visit[i] = 1
+                    if dfs(k, i + 1, sum + nums[i], cnt + 1):
+                        return True
+                    visit[i] = 0
+            return False
+
+        return dfs(k, 0, 0, 0)
