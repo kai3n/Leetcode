921043acb67aa499bfaedd35d319d98bc763edb5
Mingun Pak <mingunpa@usc.edu>
2018-01-16 19:20:15 -0800

    Optimized Solution

--- LeetCode/77. Combinations.py
+++ LeetCode/77. Combinations.py
@@ -1,3 +1,4 @@
+#Time limit solution
 class Solution(object):
     def combine(self, n, k):
         """
@@ -23,3 +24,24 @@ class Solution(object):
                     v.remove(i)
         helper(l, set(), [], k)
         return list(res)
+
+# Optimized solution
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(range(1, n + 1), k, [], res)
+        return res
+
+    def dfs(self, nums, k, path, res):
+        if k == 0:
+            res.append(path)
+            return
+        if len(nums) >= k:
+            for i in range(len(nums)):
+                self.dfs(nums[i + 1:], k - 1, path + [nums[i]], res)
+        return
