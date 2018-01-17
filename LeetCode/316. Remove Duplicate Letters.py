b43d429bc034d653672775b1340c506b5da5db4c
Mingun Pak <mingunpa@usc.edu>
2018-01-16 16:16:44 -0800

    Good Solution. but difficult to come up with

---
+++ LeetCode/316. Remove Duplicate Letters.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        for c in sorted(set(s)):
+            suffix = s[s.index(c):]
+            if set(suffix) == set(s):
+                return c + self.removeDuplicateLetters(suffix.replace(c, ''))
+        return ''
