c339d4d2fa66dc5f3a46f5aeee14ebb00077656d
Mingun Pak <mingunpa@usc.edu>
2018-01-19 21:03:35 -0800

    just send it to the end

---
+++ LeetCode/27. Remove Element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start], nums[end] = nums[end], nums[start]
+                end -= 1
+            else:
+                start += 1
+        return start
+
