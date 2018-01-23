4111d59e7b742fdfd3c09cd150587318cd61c461
Mingun Pak <mingunpa@usc.edu>
2018-01-23 12:34:41 -0800

    optimized solution

--- LeetCode/18. 4Sum.py
+++ LeetCode/18. 4Sum.py
@@ -22,3 +22,45 @@ class Solution:
                     else:
                         r -= 1
         return res
+
+# Optimized Solution
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        for i in range(len(nums)-3):
+            if nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target:
+                break
+            if nums[i]+nums[-1]+nums[-2]+nums[-3]<target:
+                continue
+            if 0 < i and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)-2):
+                if nums[i]+nums[j]+nums[j+1]+nums[j+2]>target:
+                    break
+                if nums[i]+nums[j]+nums[-1]+nums[-2]<target:
+                    continue
+                if i+1 < j and nums[j] == nums[j-1]:
+                    continue
+                l = j+1
+                r = len(nums) - 1
+                while l < r:
+                    s = nums[i] + nums[j] + nums[l] + nums[r]
+                    if s == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l+1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r-1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+        return res
