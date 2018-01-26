8ae87bae1d0f83a1b4967d1b1c86a4147fd7e9fc
Mingun Pak <mingunpa@usc.edu>
2018-01-25 18:19:21 -0800

    beautiful idea.counter (row, col, row+col, row-col)

---
+++ LeetCode/348. Design Tic-Tac-Toe.py
@@ -0,0 +1,12 @@
+class TicTacToe(object):
+    def __init__(self, n):
+        self.count = collections.Counter()
+        self.n = n
+
+    def move(self, row, col, player):
+        for i, x in enumerate((row, col, row + col, row - col)):
+            print(i, x, player)
+            self.count[i, x, player] += 1
+            if self.count[i, x, player] == self.n:
+                return player
+        return 0
