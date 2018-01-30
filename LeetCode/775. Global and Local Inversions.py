273a28d78c8e8c29cbf6e8daeaebe6b6f7c3d7af
Mingun Pak <mingunpa@usc.edu>
2018-01-29 16:04:24 -0800

    TLE

---
+++ LeetCode/775. Global and Local Inversions.py
@@ -0,0 +1,21 @@
+# TLE
+
+class Solution:
+    def isIdealPermutation(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        g_num = 0
+        l_num = 0
+
+        for i in range(len(A)):
+            for j in range(i + 1, len(A)):
+                if A[i] > A[j]:
+                    g_num += 1
+
+        for i in range(len(A) - 1):
+            if A[i] > A[i + 1]:
+                l_num += 1
+
+        return g_num == l_num
