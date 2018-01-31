88183c82f2fc56ed2554a24c65125e1e09311a31
Mingun Pak <mingunpa@usc.edu>
2018-01-30 23:23:32 -0800

    priority queue solution

--- LeetCode/767. Reorganize String.py
+++ LeetCode/767. Reorganize String.py
@@ -4,3 +4,25 @@ class Solution:
         h = len(a) // 2
         a[1::2], a[::2] = a[:h], a[h:]
         return ''.join(a) * (a[-1:] != a[-2:-1])
+
+#priority queue solution
+from collections import Counter
+import heapq
+class Solution:
+    def reorganizeString(self, S):
+        res = ""
+        pq = []
+        c = Counter(S)
+        for key, value in c.items():
+            heapq.heappush(pq, (-value, key))
+        p_a, p_b = 0, ''
+        while pq:
+            a, b = heapq.heappop(pq)
+            res += b
+            if p_a < 0:
+                heapq.heappush(pq, (p_a, p_b))
+            a += 1
+            p_a, p_b = a, b
+
+        if len(res) != len(S): return ""
+        return res
