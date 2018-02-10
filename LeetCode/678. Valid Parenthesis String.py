f4cb9ae732d3e6fec93252ac1c2108713bc6b62f
Mingun Pak <mingunpa@usc.edu>
2018-02-10 10:53:28 -0800

    linear solution

--- LeetCode/678. Valid Parenthesis String.py
+++ LeetCode/678. Valid Parenthesis String.py
@@ -26,3 +26,22 @@ class Solution:
                     return False
 
         return helper(s, 0, 0)
+
+# O(n)
+
+class Solution:
+    def checkValidString(self, s):
+        cmin = cmax = 0
+        for i in s:
+            if i == '(':
+                cmax += 1
+                cmin += 1
+            if i == ')':
+                cmax -= 1
+                cmin = max(cmin - 1, 0)
+            if i == '*':
+                cmax += 1
+                cmin = max(cmin - 1, 0)
+            if cmax < 0:
+                return False
+        return cmin == 0
