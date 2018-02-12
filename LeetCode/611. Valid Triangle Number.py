c895afc58038454ba90d0e286f5c7cc376feebf7
Mingun Pak <mingunpa@usc.edu>
2018-02-11 23:29:59 -0800

    sort and two point

--- LeetCode/611. Valid Triangle Number.py
+++ LeetCode/611. Valid Triangle Number.py
@@ -14,3 +14,26 @@ class Solution:
                     if nums[i] + nums[j] > nums[k]:
                         count += 1
         return count
+
+
+# AT
+
+class Solution:
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        nums.sort()
+
+        for i in range(len(nums) - 1, 1, -1):
+            left = 0
+            right = i - 1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return count
