d01338fcacf2fc036392d4251c8c207cbc519ff4
Mingun Pak <mingunpa@usc.edu>
2018-02-11 23:11:41 -0800

    TLE

---
+++ LeetCode/611. Valid Triangle Number.py
@@ -0,0 +1,16 @@
+# TLE
+class Solution:
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] > nums[k]:
+                        count += 1
+        return count
