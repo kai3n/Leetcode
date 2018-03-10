f7e875267a8c402becb63aa55875f2c508e1504a
Mingun Pak <mingunpa@usc.edu>
2018-03-10 00:37:46 -0800

    simple DFS

---
+++ LeetCode/323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countComponents(self, n, edges):
+        def dfs(n, g, visited):
+            if visited[n]:
+                return
+            visited[n] = 1
+            for x in g[n]:
+                dfs(x, g, visited)
+
+        visited = [0] * n
+        g = {x: [] for x in range(n)}
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+
+        ret = 0
+        for i in range(n):
+            if not visited[i]:
+                dfs(i, g, visited)
+                ret += 1
+
+        return ret
