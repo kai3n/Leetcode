9050970266343efaf5e1f96fe42bafb4f659817b
Mingun Pak <mingunpa@usc.edu>
2018-10-09 18:25:28 -0700

    easy

---
+++ LeetCode/832. Flipping an Image.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        return [[1 - i for i in row[::-1]] for row in A]
+
