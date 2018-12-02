023cb99e88424bdffdf08c3b7d5cf2877a0c3c46
Mingun Pak <mingunpa@usc.edu>
2018-12-01 20:35:04 -0800

    irritated

--- LeetCode/951. Flip Equivalent Binary Trees.py
+++ LeetCode/951. Flip Equivalent Binary Trees.py
@@ -1,10 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution(object):
     def flipEquiv(self, root1, root2):
         """
@@ -12,47 +5,10 @@ class Solution(object):
         :type root2: TreeNode
         :rtype: bool
         """
-
-        def helper(root1, root2):
-            if root1 and root2:
-                if root1.val != root2.val:
-                    return False
-
-                if root1.left and root2.left and root1.right and root2.right:
-                    if root1.left.val == root2.left.val and root1.right.val == root2.right.val:
-                        return helper(root1.left, root2.left) and helper(root1.right, root2.right)
-                    elif root1.left.val == root2.right.val and root1.right.val == root2.left.val:
-                        return helper(root1.left, root2.right) and helper(root1.right, root2.left)
-                    else:
-                        return False
-                elif not root1.left and not root2.left and root1.right and root2.right:
-                    if root1.right.val == root2.right.val:
-                        return helper(root1.right, root2.right)
-                    else:
-                        return False
-                elif root1.left and root2.left and not root1.right and not root2.right:
-                    if root1.left.val == root2.left.val:
-                        return helper(root1.left, root2.left)
-                    else:
-                        return False
-                elif root1.left and not root2.left and not root1.right and root2.right:
-                    if root1.left.val == root2.right.val:
-                        return helper(root1.left, root2.right)
-                    else:
-                        return False
-                elif not root1.left and root2.left and root1.right and not root2.right:
-                    if root1.right.val == root2.left.val:
-                        return helper(root1.right, root2.left)
-                    else:
-                        return False
-                elif not root1.left and not root2.left and not root1.right and not root2.right:
-                    return True
-                else:
-                    return False
-            elif root1 and not root2:
-                return False
-            elif not root1 and root2:
-                return False
-            return True
-
-        return helper(root1, root2)
+        if not root1:
+            return not root2
+        if not root2:
+            return not root1
+        if root1.val != root2.val:
+            return False
+        return (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)) or (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left))
