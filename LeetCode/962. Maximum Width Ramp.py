ffe7f4a972d7b878a077c3ddcf90eda9cbcb6843
Mingun Pak <mingunpa@usc.edu>
2019-01-15 06:23:34 -0800

    O(n) time complexity by using stack

--- LeetCode/962. Maximum Width Ramp.py
+++ LeetCode/962. Maximum Width Ramp.py
@@ -1,12 +1,24 @@
 class Solution(object):
+    # def maxWidthRamp(self, A):
+    #     ans = 0
+    #     m = float('inf')
+    #     for i in sorted(range(len(A)), key = A.__getitem__):
+    #         print(i)
+    #         ans = max(ans, i - m)
+    #         m = min(m, i)
+    #     return ans
+
     def maxWidthRamp(self, A):
-        ans = 0
-        m = float('inf')
-        for i in sorted(range(len(A)), key = A.__getitem__):
-            print(i)
-            ans = max(ans, i - m)
-            m = min(m, i)
-        return ans
+        s = []
+        res = 0
+        for i, a in enumerate(A):
+            if not s or A[s[-1]] > a:
+                s.append(i)
+        print(s)
+        for j in range(len(A))[::-1]:
+            while s and A[s[-1]] <= A[j]:
+                res = max(res, j - s.pop())
+        return res
 
 solution = Solution()
 print(solution.maxWidthRamp([6,0,8,2,1,5]))
