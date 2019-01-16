46986ef428ae74439655b5c9380805fef159853b
Mingun Pak <mingunpa@usc.edu>
2019-01-15 19:31:07 -0800

    Counter

--- LeetCode/954. Array of Doubled Pairs.py
+++ LeetCode/954. Array of Doubled Pairs.py
@@ -1,9 +1,14 @@
 class Solution(object):
     def canReorderDoubled(self, A):
+        import collections
         count = collections.Counter(A)
+        print(sorted(A, key = abs))
         for x in sorted(A, key = abs):
             if count[x] == 0: continue
             if count[2*x] == 0: return False
             count[x] -= 1
             count[2*x] -= 1
         return True
+
+solution = Solution()
+print(solution.canReorderDoubled([-2,4,2,-4]))
