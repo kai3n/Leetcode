3b64c0064583646f378b205787c59003eef22b87
Mingun Pak <mingunpa@usc.edu>
2019-01-17 08:34:28 -0800

    O(logN**2)

--- LeetCode/222. Count Complete Tree Nodes.py
+++ LeetCode/222. Count Complete Tree Nodes.py
@@ -22,3 +22,19 @@ class Solution:
 
         traversal(root)
         return self.count
+
+class Solution:
+    def countNodes(self, root):
+        if not root:
+            return 0
+        leftDepth = self.getDepth(root.left)
+        rightDepth = self.getDepth(root.right)
+        if leftDepth == rightDepth:
+            return pow(2, leftDepth) + self.countNodes(root.right)
+        else:
+            return pow(2, rightDepth) + self.countNodes(root.left)
+
+    def getDepth(self, root):
+        if not root:
+            return 0
+        return 1 + self.getDepth(root.left)
