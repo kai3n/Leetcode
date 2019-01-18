01ce8efcd8934c2afaf95fa9c8c4eb92c060eb8e
Mingun Pak <mingunpa@usc.edu>
2019-01-17 20:51:40 -0800

    dirty code

---
+++ LeetCode/929. Unique Email Addresses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        res = set()
+        for i in range(len(emails)):
+            before_at = True
+            met_plus = False
+            candidate = []
+            j = 0
+            while j < len(emails[i]):
+                if emails[i][j] == "+":
+                    while emails[i][j] != "@":
+                        j += 1
+
+                if emails[i][j] == "@":
+                    candidate += emails[i][j]
+                    before_at = False
+                    j += 1
+                    continue
+                if before_at and emails[i][j] == ".":
+                    j += 1
+                    continue
+                candidate += emails[i][j]
+                j += 1
+            res.add(''.join(candidate))
+        print(res)
+        return len(res)
