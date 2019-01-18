568b9c8d394949d22906be98ec4ec3e9cb6ee242
Mingun Pak <mingunpa@usc.edu>
2019-01-17 20:52:51 -0800

    add clear solution

--- LeetCode/929. Unique Email Addresses.py
+++ LeetCode/929. Unique Email Addresses.py
@@ -28,3 +28,14 @@ class Solution:
             res.add(''.join(candidate))
         print(res)
         return len(res)
+
+
+class Solution:
+    def numUniqueEmails(self, emails):
+        seen = set()
+        for email in emails:
+            local, domain = email.split('@')
+            if '+' in local:
+                local = local[:local.index('+')]
+            seen.add(local.replace('.','') + '@' + domain)
+        return len(seen)
