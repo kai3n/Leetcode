c507e618f2195bbc23c74dddd1b5a3d3b735f23c
Mingun Pak <mingunpa@usc.edu>
2019-01-18 22:14:03 -0800

    useless

---
+++ LeetCode/833. Find And Replace in String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findReplaceString(self, S, indexes, sources, targets):
+        """
+        :type S: str
+        :type indexes: List[int]
+        :type sources: List[str]
+        :type targets: List[str]
+        :rtype: str
+        """
+        for i, s, t in sorted(zip(indexes, sources, targets), reverse=True):
+            S = S[:i] + t + S[i + len(s):] if S[i:i + len(s)] == s else S
+        return S
