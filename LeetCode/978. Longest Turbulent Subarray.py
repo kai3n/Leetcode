7bb01f305754719ffa225ee6c0e3f11edf317d90
Mingun Pak <mingunpa@usc.edu>
2019-01-19 20:12:56 -0800

    clear solution

--- LeetCode/978. Longest Turbulent Subarray.py
+++ LeetCode/978. Longest Turbulent Subarray.py
@@ -1,6 +1,4 @@
 class Solution:
-    res = 0
-
     def maxTurbulenceSize(self, A):
         """
         :type A: List[int]
@@ -8,44 +6,20 @@ class Solution:
         """
         if not A:
             return 0
-        elif len(A) == 1:
-            return 1
 
-        def helper(end, flag):
-            i = end
-            begin = end - 1
+        ret = 1
+        down, upwards = 1, 1
+        for i in range(1, len(A)):
+            if A[i] > A[i - 1]:
+                upwards = down + 1
+                ret = max(ret, upwards)
+                down = 1
+            elif A[i] < A[i - 1]:
+                down = upwards + 1
+                ret = max(ret, down)
+                upwards = 1
+            else:
+                down = 1
+                upwards = 1
 
-            while i < len(A):
-                if flag:
-                    if A[i - 1] > A[i]:
-                        flag = False
-                        end += 1
-                    else:
-                        self.res = max(self.res, end - begin)
-                        begin = i - 1
-                else:
-                    if A[i - 1] < A[i]:
-                        flag = True
-                        end += 1
-                    else:
-                        self.res = max(self.res, end - begin)
-                        begin = i - 1
-
-                flag2 = True
-                while end < len(A) and begin+1 == end and A[begin] == A[end]:
-                    begin += 1
-                    end += 1
-                    i += 1
-                    flag2 = False
-
-                if flag2:
-                    i += 1
-                    end = i
-
-            self.res = max(self.res, end - begin)
-        helper(1, True)
-
-        return self.res
-
-s = Solution()
-print(s.maxTurbulenceSize([0,1,1,0,1,0,1,1,0,0]))
+        return ret
