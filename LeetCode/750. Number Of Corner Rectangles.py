936f39648f7499db5e0d7b5427e4388231e8c26c
Mingun Pak <mingunpa@usc.edu>
2019-01-22 21:29:44 -0800

    brute force

---
+++ LeetCode/750. Number Of Corner Rectangles.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countCornerRectangles(self, grid):
+        import collections
+        count = collections.Counter()
+        ans = 0
+        for row in grid:
+            for c1, v1 in enumerate(row):
+                if v1:
+                    for c2 in range(c1+1, len(row)):
+                        if row[c2]:
+                            ans += count[c1, c2]
+                            count[c1, c2] += 1
+        return ans
