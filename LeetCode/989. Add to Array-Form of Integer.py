ff2fc4bf53eb6cb914c000b5a6fa952859cd3015
Mingun Pak <mingunpa@usc.edu>
2019-02-09 19:06:59 -0800

    easy

---
+++ LeetCode/989. Add to Array-Form of Integer.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addToArrayForm(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: List[int]
+        """
+        return map(int, str(int(''.join(map(str, A))) + K))
