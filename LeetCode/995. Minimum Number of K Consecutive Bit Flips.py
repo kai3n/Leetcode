f07a49681d2cf2f6444a404d731f6c943163910b
Mingun Pak <mingunpa@usc.edu>
2019-02-19 21:20:40 -0800

    hard:flip trick

--- LeetCode/995. Minimum Number of K Consecutive Bit Flips.py
+++ LeetCode/995. Minimum Number of K Consecutive Bit Flips.py
@@ -25,3 +25,20 @@ class Solution(object):
             if A[i] == 0:
                 return -1
         return count
+
+class Solution(object):
+    def minKBitFlips(self, A, K):
+        N = len(A)
+        hint = [0] * N
+        ans = flip = 0
+
+        for i, x in enumerate(A):
+            flip ^= hint[i]
+            if x ^ flip == 0:
+                ans += 1
+                if i + K > N:
+                    return -1
+                flip ^= 1
+                if i + K < N:
+                    hint[i + K] = 1
+        return ans
