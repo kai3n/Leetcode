3aa615d4f83ed97b4b8fae9f49d6c05d74a884c2
Mingun Pak <mingunpa@usc.edu>
2019-03-25 23:41:53 -0700

    try again

---
+++ LeetCode/1015. Numbers With Repeated Digits.py
@@ -0,0 +1,48 @@
+# given number n, see whether n has repeated number
+def has_repeated(n):
+    str_n = str(n)
+    return len(set(str_n)) != len(str_n)
+
+def permutation(n, k):
+    prod = 1
+    for i in range(k):
+        prod *= (n-i)
+    return prod
+
+# calculate number of non-repeated n-digit numbers
+# note: the n-digit number can't start with 0
+# i.e: n_digit_no_repeat(2) calculates the non-repeated
+#   numbers in range [10, 99] (inclusive)
+def n_digit_no_repeat(n):
+    if n == 1:
+        return 9
+    else:
+        return  9 * permutation(9, n-1)
+
+class Solution(object):
+    def numDupDigitsAtMostN(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        N_str = str(N)
+        n_digit = len(N_str)
+        digits = map(int, N_str)
+        result = N - 1
+        prefix = 0
+        for i in range(1, n_digit):
+            result -= n_digit_no_repeat(i)
+        for i in range(n_digit):
+            # when we fix the most significant digit, it
+            # can't be zero
+            start = 0 if i else 1
+            for j in range(start, digits[i]):
+                if has_repeated(prefix * 10 + j):
+                    continue
+                if i != n_digit-1:
+                    result -= permutation(10-i-1, n_digit-1-i)
+                else:
+                    # optmized from `result -= has_repeated(prefix*10+j)`
+                    result -= 1
+            prefix = prefix*10 + digits[i]
+        return result + has_repeated(N)
