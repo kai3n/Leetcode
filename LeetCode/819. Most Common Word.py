502d1fc0c40749fe6b69393fb2cd5eea5697c8da
Mingun Pak <mingunpa@usc.edu>
2019-03-28 01:18:58 -0700

    counter

---
+++ LeetCode/819. Most Common Word.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def mostCommonWord(self, p, banned):
+        ban = set(banned)
+        words = re.findall(r'\w+', p.lower())
+        return collections.Counter(w for w in words if w not in ban).most_common(1)[0][0]
