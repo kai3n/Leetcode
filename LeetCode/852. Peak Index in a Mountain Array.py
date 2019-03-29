72e090acc527463f3757dea10b6597a141932f38
Mingun Pak <mingunpa@usc.edu>
2019-03-29 00:32:46 -0700

    easy

---
+++ LeetCode/852. Peak Index in a Mountain Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for i in range(1, len(A)-1):
+            if A[i-1] < A[i] > A[i+1]:
+                return i
+        return -1
