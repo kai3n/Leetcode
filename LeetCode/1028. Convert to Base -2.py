24acae91b028eef415aac7ff133b4f92adc98286
Mingun Pak <mingunpa@usc.edu>
2019-03-30 23:21:35 -0700

    simulation

---
+++ LeetCode/1028. Convert to Base -2.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def baseNeg2(self, x):
+        res = []
+        while x:
+            res.append(x & 1)
+            x = -(x >> 1)
+        return "".join(map(str, res[::-1] or [0]))
+    def base2(self, x):
+        res = []
+        while x:
+            res.append(x & 1)
+            x = x >> 1
+        return "".join(map(str, res[::-1] or [0]))
