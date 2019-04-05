cf7ffa0bbecd1d378e1336690ba55cc87af1fce6
Mingun Pak <mingunpa@usc.edu>
2019-04-04 22:56:41 -0700

    easy

---
+++ LeetCode/700. Search in a Binary Search Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        while root:
+            if root.val == val:
+                return root
+            elif root.val > val:
+                root = root.left
+            else:
+                root = root.right
+        return None
