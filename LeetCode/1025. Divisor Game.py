000b5f5516d6a13f5d22c034d88e7dcc368f1882
Mingun Pak <mingunpa@usc.edu>
2019-04-14 01:10:29 -0700

    weird

--- LeetCode/1025. Divisor Game.py
+++ LeetCode/1025. Divisor Game.py
@@ -14,3 +14,11 @@ class Solution(object):
             N -= x
             winner ^= True
         return winner
+
+class Solution(object):
+    def divisorGame(self, N):
+        """
+        :type N: int
+        :rtype: bool
+        """
+        return N % 2 == 0
