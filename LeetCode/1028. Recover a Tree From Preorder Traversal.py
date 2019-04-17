8782a46c681a1efb8704f02087a547098a727697
Mingun Pak <mingunpa@usc.edu>
2019-04-16 21:59:20 -0700

    stack

---
+++ LeetCode/1028. Recover a Tree From Preorder Traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def recoverFromPreorder(self, S):
+        stack, i = [], 0
+        while i < len(S):
+            level, val = 0, ""
+            while i < len(S) and S[i] == '-':
+                level, i = level + 1, i + 1
+            while i < len(S) and S[i] != '-':
+                val, i = val + S[i], i + 1
+            while len(stack) > level:
+                stack.pop()
+            node = TreeNode(val)
+            if stack and stack[-1].left is None:
+                stack[-1].left = node
+            elif stack:
+                stack[-1].right = node
+            stack.append(node)
+        return stack[0]
