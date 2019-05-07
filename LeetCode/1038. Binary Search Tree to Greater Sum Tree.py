80bbde761889d9114961fff46965fb7dbc9f8626
Mingun Pak <mingunpa@usc.edu>
2019-05-06 22:29:51 -0700

    reverse_preorder

---
+++ LeetCode/1038. Binary Search Tree to Greater Sum Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def bstToGst(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        accu_sum = [0]
+        def helper(root, accu_sum):
+            if root:
+                helper(root.right, accu_sum)
+                accu_sum[0] += root.val
+                root.val = accu_sum[0]
+                helper(root.left, accu_sum)
+        helper(root, accu_sum)
+        return root
