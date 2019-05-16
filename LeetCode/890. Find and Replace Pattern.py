73bb042bfc05875e8ec669cf43d6040efee01274
Mingun Pak <mingunpa@usc.edu>
2019-05-15 23:37:39 -0700

    two map

---
+++ LeetCode/890. Find and Replace Pattern.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findAndReplacePattern(self, words, pattern):
+        """
+        :type words: List[str]
+        :type pattern: str
+        :rtype: List[str]
+        """
+        def match(word):
+            m1, m2 = {}, {}
+            for w, p in zip(word, pattern):
+                if w not in m1:
+                    m1[w] = p
+                if p not in m2:
+                    m2[p] = w
+                if (m1[w], m2[p]) != (p, w):
+                    return False
+            return True
+        return filter(match, words)
