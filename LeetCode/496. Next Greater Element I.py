08195f1c79bfd39dc8850a629f0a542b9e967816
Mingun Pak <mingunpa@usc.edu>
2019-05-23 01:13:27 -0700

    stack solution

--- LeetCode/496. Next Greater Element I.py
+++ LeetCode/496. Next Greater Element I.py
@@ -13,3 +13,14 @@ class Solution(object):
                     res[i] = nums2[j]
                     break
         return res
+
+
+class Solution:
+    def nextGreaterElement(self, findNums, nums):
+        st, d = [], {}
+        for n in nums:
+            while st and st[-1] < n:
+                d[st.pop()] = n
+            st.append(n)
+
+        return [d.get(x, -1) for x in findNums]
