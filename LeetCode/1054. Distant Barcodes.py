603ef0f1875d9bd53f600b18b59520f80c558ed9
Mingun Pak <mingunpa@usc.edu>
2019-05-27 19:33:15 -0700

    short version

--- LeetCode/1054. Distant Barcodes.py
+++ LeetCode/1054. Distant Barcodes.py
@@ -9,3 +9,10 @@ class Solution(object):
                 if i >= n:
                     i = 1
         return res
+
+class Solution(object):
+    def rearrangeBarcodes(self, A):
+        count = collections.Counter(A)
+        A.sort(key=lambda a: (count[a], a))
+        A[1::2], A[::2] = A[0:len(A) / 2], A[len(A) / 2:]
+        return A
