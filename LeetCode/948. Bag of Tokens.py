63722f82368c0ca40d094e910804ae74b75449b1
Mingun Pak <mingunpa@usc.edu>
2019-05-31 17:07:47 -0700

    greedy

---
+++ LeetCode/948. Bag of Tokens.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def bagOfTokensScore(self, tokens, P):
+        """
+        :type tokens: List[int]
+        :type P: int
+        :rtype: int
+        """
+        tokens.sort()
+        deque = collections.deque(tokens)
+        ans = bns = 0
+        while deque and (P >= deque[0] or bns):
+            while deque and P >= deque[0]:
+                P -= deque.popleft()
+                bns += 1
+            ans = max(ans, bns)
+
+            if deque and bns:
+                P += deque.pop()
+                bns -= 1
+        return ans
