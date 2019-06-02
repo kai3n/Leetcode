7eac895f0efe850369975886dc2ee10f6ffe44d6
Mingun Pak <mingunpa@usc.edu>
2019-06-01 22:13:25 -0700

    add binary search

--- LeetCode/1064. Fixed Point.py
+++ LeetCode/1064. Fixed Point.py
@@ -8,3 +8,23 @@ class Solution(object):
             if A[i] == i:
                 return i
         return -1
+
+
+class Solution(object):
+    def fixedPoint(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(A) - 1
+
+        while left <= right:
+            mid = (left + right) / 2
+            if A[mid] == mid:
+                return mid
+            elif A[mid] > mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
