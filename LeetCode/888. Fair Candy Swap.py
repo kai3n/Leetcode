f3eed1441515ce8f134c1e1d8243244eb8dc4eb7
Mingun Pak <mingunpa@usc.edu>
2019-06-23 22:49:36 -0700

    hashmap

--- LeetCode/888. Fair Candy Swap.py
+++ LeetCode/888. Fair Candy Swap.py
@@ -29,3 +29,17 @@ class Solution(object):
             if r != -1:
                 return [A[i], B[r]]
 
+
+class Solution(object):
+    def fairCandySwap(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        dif = (sum(A) - sum(B)) / 2
+        A = set(A)
+        for b in set(B):
+            if dif + b in A:
+                return [dif + b, b]
+
