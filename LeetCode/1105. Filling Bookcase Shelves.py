0137e7906b067c84594f864353c77effc374b06e
Mingun Pak <mingunpa@usc.edu>
2019-07-06 13:30:26 -0700

    dp

---
+++ LeetCode/1105. Filling Bookcase Shelves.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minHeightShelves(self, books, shelf_width):
+        """
+        :type books: List[List[int]]
+        :type shelf_width: int
+        :rtype: int
+        """
+        n = len(books)
+        dp = [float('inf') for _ in range(n + 1)]
+        dp[0] = 0
+        for i in range(1, n + 1):
+            max_width = shelf_width
+            max_height = 0
+            j = i - 1
+            while j >= 0 and max_width - books[j][0] >= 0:
+                max_width -= books[j][0]
+                max_height = max(max_height, books[j][1])
+                dp[i] = min(dp[i], dp[j] + max_height)
+                j -= 1
+        return dp[-1]
