bbc883ba514e0b9ab894e5773e9a162e93c9e60e
Mingun Pak <mingunpa@usc.edu>
2019-07-09 00:02:28 -0700

    priority queue

--- LeetCode/743. Network Delay Time.py
+++ LeetCode/743. Network Delay Time.py
@@ -12,3 +12,21 @@ class Solution:
                     q.append((time + w, v))
         mx = max(t)
         return mx if mx < float("inf") else -1
+
+class Solution(object):
+    def networkDelayTime(self, times, N, K):
+        graph = collections.defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        pq = [(0, K)]
+        dist = {}
+        while pq:
+            d, node = heapq.heappop(pq)
+            if node in dist: continue
+            dist[node] = d
+            for nei, d2 in graph[node]:
+                if nei not in dist:
+                    heapq.heappush(pq, (d+d2, nei))
+
+        return max(dist.values()) if len(dist) == N else -1
