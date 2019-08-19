f096b4538d5100241e63f9055cd271a0e820bc4b
Mingun Pak <mingunpa@usc.edu>
2019-08-18 17:10:38 -0700

    string

---
+++ LeetCode/859. Buddy Strings.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def buddyStrings(self, A, B):
+        if len(A) != len(B): return False
+        if A == B and len(set(A)) < len(A): return True
+        dif = [(a, b) for a, b in zip(A, B) if a != b]
+        return len(dif) == 2 and dif[0] == dif[1][::-1]
