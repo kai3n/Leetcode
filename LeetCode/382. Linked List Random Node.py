3a2feaf7d9ffb9833e1927d5ae77c5f903990006
Mingun Pak <mingunpa@usc.edu>
2019-08-21 22:01:14 -0700

    probability solution

--- LeetCode/382. Linked List Random Node.py
+++ LeetCode/382. Linked List Random Node.py
@@ -38,3 +38,26 @@ class Solution(object):
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(head)
 # param_1 = obj.getRandom()
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+
+    def __init__(self, head):
+        self.head = head
+
+    def getRandom(self):
+        result, node, index = self.head, self.head.next, 1
+        while node:
+            if random.randint(0, index) is 0:
+                result = node
+            node = node.next
+            index += 1
+        return result.val
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
