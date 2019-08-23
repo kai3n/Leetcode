7130fc0024a5a080d89de553ca3931fd0277ecad
Mingun Pak <mingunpa@usc.edu>
2019-08-22 21:21:51 -0700

    advanced binary search

---
+++ LeetCode/658. Find K Closest Elements.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        first = 0
+        last = len(arr) - k
+        while first < last:
+            mid = (first+last)//2
+            if x-arr[mid] > arr[mid+k]-x:
+                first = mid + 1
+            else:
+                last = mid
+        return arr[first:first+k]
