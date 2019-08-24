d421103b7857664b9fc052d15cc6f2d922b46b4e
Mingun Pak <mingunpa@usc.edu>
2019-08-24 15:38:56 -0700

    using reverse function

---
+++ LeetCode/25. Reverse Nodes in k-Group.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        count, node = 0, head
+        while node and count < k:
+            node = node.next
+            count += 1
+        if count < k: return head
+        new_head, prev = self.reverse(head, count)
+        head.next = self.reverseKGroup(new_head, k)
+        return prev
+
+    def reverse(self, head, count):
+        prev, cur, nxt = None, head, head
+        while count > 0:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+            count -= 1
+        return (cur, prev)
