0752ad04fe15d128893701f7dee40ccc6292a4b1
Mingun Pak <mingunpa@usc.edu>
2019-08-25 16:36:47 -0700

    linear solution - greedy

--- LeetCode/45. Jump Game II.py
+++ LeetCode/45. Jump Game II.py
@@ -11,3 +11,18 @@ class Solution(object):
                 if i + j < len(nums):
                     dp[i + j] = min(dp[i + j], dp[i]+1)
         return dp[-1]
+
+
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        cur_end = cur_farthest = jump = 0
+        for i in range(len(nums)-1):
+            cur_farthest = max(cur_farthest, nums[i]+i)
+            if cur_end == i:
+                cur_end = cur_farthest
+                jump += 1
+        return jump
