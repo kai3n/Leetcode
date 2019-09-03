e2bcdfc1cd05a4401fe825e76a331ebb37885618
Mingun Pak <mingunpa@usc.edu>
2019-09-02 22:20:23 -0700

    simple solution

--- LeetCode/366. Find Leaves of Binary Tree.py
+++ LeetCode/366. Find Leaves of Binary Tree.py
@@ -46,4 +46,18 @@ root.left.right = TreeNode(5)
 test = Solution()
 print(test.findLeaves(root))
 
-
+class Solution:
+    def findLeaves(self, root: TreeNode) -> List[List[int]]:
+        def order(root, dic):
+            if not root:
+                return 0
+            left = order(root.left, dic)
+            right = order(root.right, dic)
+            lev = max(left, right) + 1
+            dic[lev] += root.val,
+            return lev
+        dic, ret = collections.defaultdict(list), []
+        order(root, dic)
+        for i in range(1, len(dic) + 1):
+            ret.append(dic[i])
+        return ret
