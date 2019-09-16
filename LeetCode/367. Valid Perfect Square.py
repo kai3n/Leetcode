ff30ca7bb45ace2cc56b805712750bc6529813b5
Mingun Pak <mingunpa@usc.edu>
2019-09-15 20:51:47 -0700

    Add binary search solution

--- LeetCode/367. Valid Perfect Square.py
+++ LeetCode/367. Valid Perfect Square.py
@@ -14,7 +14,20 @@ test = Solution()
 print(test.isPerfectSquare(256))
 #  Babylonian method
 
-
-
-
+"""
+class Solution(object):
+    def isPerfectSquare(self, num):
+        left = 0
+        right = num
+        
+        while left <= right:
+            mid = left + (right-left)//2
+            if  mid ** 2 == num:
+                return True
+            elif mid ** 2 > num:
+                right = mid -1
+            else:
+                left = mid +1
+        return False
+"""
 
