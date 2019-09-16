e6cdf81fb493715e00fba1919b38e0e00a8dc920
Mingun Pak <mingunpa@usc.edu>
2019-09-15 20:45:02 -0700

    Add binary search solution

--- LeetCode/69. Sqrt(x).py
+++ LeetCode/69. Sqrt(x).py
@@ -8,3 +8,25 @@ class Solution(object):
         while r*r > x:
             r = (r + x/r) / 2
         return r
+
+
+"""
+class Solution(object):
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+        
+        left, right = 2, x // 2
+        
+        while left <= right:
+            pivot = left + (right - left) // 2
+            num = pivot * pivot
+            if num > x:
+                right = pivot -1
+            elif num < x:
+                left = pivot + 1
+            else:
+                return pivot
+            
+        return right
+"""
