5fefa29bcbea3d9a212a51fe1aeadce97e34533b
Mingun Pak <mingunpa@usc.edu>
2019-09-17 22:09:43 -0700

    Add DFS solution

--- LeetCode/261. Graph Valid Tree.py
+++ LeetCode/261. Graph Valid Tree.py
@@ -27,3 +27,22 @@ class Solution:
                 return False
             parent[x] = y
         return len(edges) == n - 1
+
+class Solution:
+    def validTree(self, n, edges):
+        dic = {i: set() for i in range(n)}
+        for i, j in edges:
+            dic[i].add(j)
+            dic[j].add(i)
+        stack = [0]
+        visited = set()
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                return False
+            visited.add(node)
+            for neighbour in dic[node]:
+                stack.append(neighbour)
+                dic[neighbour].remove(node)
+            dic.pop(node)
+        return not dic
