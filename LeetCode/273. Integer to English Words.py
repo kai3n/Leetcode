184d6d64229fc47421a9fa019c623afd74d19075
Mingun Pak <mingunpa@usc.edu>
2019-09-22 19:37:38 -0700

    easier solution

--- LeetCode/273. Integer to English Words.py
+++ LeetCode/273. Integer to English Words.py
@@ -14,3 +14,23 @@ class Solution(object):
                 if n < 1000**(p+1):
                     return words(n/1000**p) + [w] + words(n%1000**p)
         return ' '.join(words(num)) or 'Zero'
+
+class Solution(object):
+    def numberToWords(self, num):
+        to19 = 'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve ' \
+               'Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split()
+        tens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split()
+        def words(n):
+            if n < 20:
+                return to19[n-1:n]
+            if n < 100:
+                return [tens[n/10-2]] + words(n%10)
+            if n < 1000:
+                return [to19[n/100-1]] + ['Hundred'] + words(n%100)
+            if n < 1000000:
+                return words(n/1000) + ['Thousand'] + words(n%1000)
+            if n < 1000000000:
+                return words(n/1000000) + ['Million'] + words(n%1000000)
+            if n < 1000000000000:
+                return words(n/1000000000) + ['Billion'] + words(n%1000000000)
+        return ' '.join(words(num)) or 'Zero'
