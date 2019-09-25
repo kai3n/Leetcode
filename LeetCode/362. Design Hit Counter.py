4b3677407ca302a10d6671d5336816b2aa62b9d9
Mingun Pak <mingunpa@usc.edu>
2019-09-24 22:12:22 -0700

    different version

--- LeetCode/362. Design Hit Counter.py
+++ LeetCode/362. Design Hit Counter.py
@@ -26,3 +26,25 @@ class HitCounter(object):
             if timestamp - 300 < self.arr[i] <= timestamp:
                 count += 1
         return count
+
+
+class HitCounter(object):
+
+    def __init__(self):
+        self.q = [(0, 0)] * 300
+
+    def hit(self, timestamp):
+        idx = timestamp % 300
+        time, hit = self.q[idx]
+        if time != timestamp:
+            self.q[idx] = timestamp, 1
+        else:
+            self.q[idx] = time, hit + 1
+
+    def getHits(self, timestamp):
+        ans = 0
+        for i in xrange(0, len(self.q)):
+            time, hit = self.q[i]
+            if timestamp - time < 300:
+                ans += hit
+        return ans
