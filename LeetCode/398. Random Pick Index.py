46bf6fe978f060b9b5c5d82ace19a0b0d3d9402f
Mingun Pak <mingunpa@usc.edu>
2019-09-26 19:07:14 -0700

    Reservoir Sampling solution

---
+++ LeetCode/398. Random Pick Index.py
@@ -0,0 +1,15 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def pick(self, target):
+        res = None
+        count = 0
+        for i, x in enumerate(self.nums):
+            if x == target:
+                count += 1
+                chance = random.randint(1, count)
+                if chance == count:
+                    res = i
+        return res
