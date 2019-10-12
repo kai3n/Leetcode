a0b22962253eb5a11bdbf6c1a4404074d1a7ec11
Mingun Pak <mingunpa@usc.edu>
2019-10-12 01:03:39 -0700

    hashmap

--- LeetCode/1146. Snapshot Array.py
+++ LeetCode/1146. Snapshot Array.py
@@ -32,6 +32,51 @@ class SnapshotArray(object):
         """
         return self.d[snap_id][index]
 
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
+
+class SnapshotArray(object):
+
+    def __init__(self, length):
+        """
+        :type length: int
+        """
+        self.snap_id = 0
+        self.arr = [{0: 0} for _ in range(length)]
+
+    def set(self, index, val):
+        """
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+        self.arr[index][self.snap_id] = val
+
+    def snap(self):
+        """
+        :rtype: int
+        """
+        self.snap_id += 1
+        return self.snap_id - 1
+
+    def get(self, index, snap_id):
+        """
+        :type index: int
+        :type snap_id: int
+        :rtype: int
+        """
+        if self.arr[index].get(snap_id) is not None:
+            return self.arr[index][snap_id]
+        elif len(self.arr[index]) == 0:
+            return 0
+        else:
+            i = max([k for k in self.arr[index].keys() if k < snap_id])
+            return self.arr[index][i]
+
 # Your SnapshotArray object will be instantiated and called as such:
 # obj = SnapshotArray(length)
 # obj.set(index,val)
