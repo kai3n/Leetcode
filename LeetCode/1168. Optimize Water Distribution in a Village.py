d3e84a6cdfdfd68caec747d56a76711e104b6ddc
Mingun Pak <mingunpa@usc.edu>
2019-10-13 20:41:06 -0700

    prim algorithm

--- LeetCode/1168. Optimize Water Distribution in a Village.py
+++ LeetCode/1168. Optimize Water Distribution in a Village.py
@@ -18,3 +18,34 @@ class Solution(object):
                 n -= 1
             if n == 0:
                 return res
+
+
+import collections
+import heapq
+# Minimum Spanning Tree
+
+# Prim
+class Solution:
+    def minCostToSupplyWater(self, n, wells, pipes):
+        # build graph
+        graph = collections.defaultdict(list)
+        for u, v, w in pipes:
+            graph[u].append([w, v])
+            graph[v].append([w, u])
+        for i in range(n):
+            # No need to point back to 0
+            graph[0].append([wells[i], i+1])
+            # graph[i+1].append([wells[i], i+1, 0])
+        visited = {0}
+        edges = graph[0]
+        heapq.heapify(edges)
+        res = 0
+        while len(visited) < n+1 and edges:
+            w, v = heapq.heappop(edges)
+            if v not in visited:
+                res += w
+                visited.add(v)
+                for edge in graph[v]:
+                    if edge[1] not in visited:
+                        heapq.heappush(edges, edge)
+        return res
