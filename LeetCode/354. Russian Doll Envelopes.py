1970a5a56afff6bbb6d845130609768a20fa1f6b
Mingun Pak <mingunpa@usc.edu>
2019-10-27 21:50:58 -0700

    time limit solution

---
+++ LeetCode/354. Russian Doll Envelopes.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxEnvelopes(self, envelopes):
+        """
+        :type envelopes: List[List[int]]
+        :rtype: int
+        """
+        if not envelopes:
+            return 0
+        new_list = []
+        for envelope in envelopes:
+            if envelope not in new_list:
+                new_list.append(envelope)
+        envelopes = new_list
+        envelopes.sort()
+        mxa_len = 0
+        dp = [1] * len(envelopes)
+
+        for i in range(1, len(envelopes)):
+            for j in range(i):
+                if envelopes[j][0] < envelopes[i][0] and envelopes[j][1] < envelopes[i][1]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
