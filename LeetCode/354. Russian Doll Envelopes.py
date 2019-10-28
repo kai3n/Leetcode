597cce0442554db9963d9d1b2d58ab17a045151a
Mingun Pak <mingunpa@usc.edu>
2019-10-27 21:51:25 -0700

    time limit solution

--- LeetCode/354. Russian Doll Envelopes.py
+++ LeetCode/354. Russian Doll Envelopes.py
@@ -12,12 +12,9 @@ class Solution(object):
                 new_list.append(envelope)
         envelopes = new_list
         envelopes.sort()
-        mxa_len = 0
-        dp = [1] * len(envelopes)
-
+        dp = [1]*len(envelopes)
         for i in range(1, len(envelopes)):
             for j in range(i):
                 if envelopes[j][0] < envelopes[i][0] and envelopes[j][1] < envelopes[i][1]:
                     dp[i] = max(dp[i], dp[j] + 1)
         return max(dp)
-
