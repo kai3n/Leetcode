2949ef6e6c7ab71e597ab0140d3ed971dfafb342
Mingun Pak <mingunpa@usc.edu>
2019-10-27 22:23:25 -0700

    dp + binary search

--- LeetCode/354. Russian Doll Envelopes.py
+++ LeetCode/354. Russian Doll Envelopes.py
@@ -18,3 +18,19 @@ class Solution(object):
                 if envelopes[j][0] < envelopes[i][0] and envelopes[j][1] < envelopes[i][1]:
                     dp[i] = max(dp[i], dp[j] + 1)
         return max(dp)
+
+
+from bisect import bisect_left
+
+class Solution(object):
+    def maxEnvelopes(self, envelopes):
+        if not envelopes:
+            return 0
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        max_idx = 0
+        heights = [envelopes[0][1]] + [0] * (len(envelopes) - 1)
+        for e in envelopes:
+            idx = bisect_left(heights, e[1], hi=max_idx + 1)
+            heights[idx] = e[1]
+            max_idx = max(max_idx, idx)
+        return max_idx + 1
