6af758f55313cd6de36dccd49260cfa326206094
Mingun Pak <mingunpa@usc.edu>
2019-11-07 23:58:57 -0800

    O(1) space complexity solution

--- LeetCode/117. Populating Next Right Pointers in Each Node II.py
+++ LeetCode/117. Populating Next Right Pointers in Each Node II.py
@@ -37,3 +37,17 @@ class Solution(object):
             for j in range(len(res[i]) - 1):
                 res[i][j].next = res[i][j + 1]
         return root
+
+    def connect(self, node):
+        tail = dummy = TreeLinkNode(0)
+        while node:
+            tail.next = node.left
+            if tail.next:
+                tail = tail.next
+            tail.next = node.right
+            if tail.next:
+                tail = tail.next
+            node = node.next
+            if not node:
+                tail = dummy
+                node = dummy.next
