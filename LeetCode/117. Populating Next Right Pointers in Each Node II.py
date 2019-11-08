90ca7b2f34c673535227b9f627ff02538b4df338
Mingun Pak <mingunpa@usc.edu>
2019-11-07 23:22:21 -0800

    bfs

---
+++ LeetCode/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,39 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root:
+            return None
+        queue = [root]
+        res = []
+        while queue:
+            l = len(queue)
+            group = []
+            for _ in range(l):
+                node = queue.pop(0)
+                group.append(node)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(group[:])
+
+        for i in range(len(res)):
+            if len(res[i]) == 1:
+                continue
+            for j in range(len(res[i]) - 1):
+                res[i][j].next = res[i][j + 1]
+        return root
