119bfd63a4c34212ebeb24fd49ee2b51dec6516e
Mingun Pak <mingunpa@usc.edu>
2019-11-10 23:28:21 -0800

    sort by remaining distance

---
+++ LeetCode/853. Car Fleet.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def carFleet(self, target, pos, speed):
+        time = [float(target - p) / s for p, s in sorted(zip(pos, speed))]
+        res = cur = 0
+        for t in time[::-1]:
+            if t > cur:
+                res += 1
+                cur = t
+        return res
