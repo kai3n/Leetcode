875b88996d3087998a55b66b738df28c348c41e3
Mingun Pak <mingunpa@usc.edu>
2019-11-11 21:24:02 -0800

    backtracking

--- LeetCode/1219. Path with Maximum Gold.py
+++ LeetCode/1219. Path with Maximum Gold.py
@@ -6,15 +6,13 @@ class Solution(object):
         """
         self.max_gold = float('-inf')
 
-        def helper(grid, i, j, visited, path):
-            self.max_gold = max(self.max_gold, sum(path))
-            if len(path) == 25:
-                return
+        def helper(grid, i, j, visited, path_sum):
+            self.max_gold = max(self.max_gold, path_sum)
             for y, x in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
                 if 0 <= i + y < len(grid) and 0 <= j + x < len(grid[0]):
                     if (i + y, j + x) not in visited and grid[i + y][j + x]:
                         visited.add((i + y, j + x))
-                        helper(grid, i + y, j + x, visited, path + [grid[i + y][j + x]])
+                        helper(grid, i + y, j + x, visited, path_sum + grid[i + y][j + x])
                         visited.remove((i + y, j + x))
 
         for i in range(len(grid)):
@@ -22,5 +20,5 @@ class Solution(object):
                 if grid[i][j]:
                     v = set()
                     v.add((i, j))
-                    helper(grid, i, j, v, [grid[i][j]])
+                    helper(grid, i, j, v, grid[i][j])
         return self.max_gold
