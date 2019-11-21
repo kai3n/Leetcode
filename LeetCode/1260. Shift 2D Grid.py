6693d82bde9e50dadc75b05223c8905962ffd5f4
Mingun Pak <mingunpa@usc.edu>
2019-11-21 01:15:26 -0800

    brute force solution

--- LeetCode/1260. Shift 2D Grid.py
+++ LeetCode/1260. Shift 2D Grid.py
@@ -1,21 +1,14 @@
 class Solution(object):
     def shiftGrid(self, grid, k):
-        """
-        :type grid: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        k %= len(grid)*len(grid[0])
-        r = k / len(grid[0])
-        c = k % len(grid[0])
-        print(r, c)
-        new_grid = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                x = j + c
-                y = i + r
-                if x >= len(grid[0]):
-                    x -= len(grid[0])
+        n, m = len(grid), len(grid[0])
+        k %= n * m
+        r, c = k / m, k % m
+        new_grid = [[0 for _ in range(m)] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                x, y = j + c, i + r
+                if x >= m:
                     y += 1
-                new_grid[y%len(grid)][x] = grid[i][j]
+                new_grid[y % n][x % m] = grid[i][j]
         return new_grid
