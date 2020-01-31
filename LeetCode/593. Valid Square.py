09516c4cd5294a94645b86944e1a873b952b89df
Mingun Pak <mingunpa@usc.edu>
2020-01-31 00:06:57 -0800

    brute force

---
+++ LeetCode/593. Valid Square.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def validSquare(self, p1, p2, p3, p4):
+        points = [p1, p2, p3, p4]
+
+        dists = collections.Counter()
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                dists[self.getDistance(points[i], points[j])] += 1
+
+        return len(dists.values()) == 2 and 4 in dists.values() and 2 in dists.values()
+
+    def getDistance(self, p1, p2):
+        return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
