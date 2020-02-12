aace81246b93032d008ee8aa2664640f5dcd67de
Mingun Pak <mingunpa@usc.edu>
2020-02-11 20:51:34 -0800

    stack solution

--- LeetCode/946. Validate Stack Sequences.py
+++ LeetCode/946. Validate Stack Sequences.py
@@ -19,3 +19,16 @@ class Solution(object):
                     tmp.append(pushed[0])
                     return helper(pushed[1:], popped)
         return helper(pushed, popped)
+
+
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        j = 0
+        stack = []
+        for x in pushed:
+            stack.append(x)
+            while stack and j < len(popped) and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return j == len(popped)
