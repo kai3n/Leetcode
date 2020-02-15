1bc1bcd538cba653505accf73ada7e1d603628cb
Mingun Pak <mingunpa@usc.edu>
2020-02-14 21:20:34 -0800

    easier solution

--- LeetCode/737. Sentence Similarity II.py
+++ LeetCode/737. Sentence Similarity II.py
@@ -33,3 +33,27 @@ class Solution:
             if g.get(i, i) != g.get(j, j):
                 return False
         return True
+
+
+class Solution:
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        if len(words1) != len(words2):
+            return False
+        graph = collections.defaultdict(list)
+        for w1, w2 in pairs:
+            graph[w1].append(w2)
+            graph[w2].append(w1)
+
+        for w1, w2 in zip(words1, words2):
+            stack, seen = [w1], {w1}
+            while stack:
+                word = stack.pop()
+                if word == w2:
+                    break
+                for nei in graph[word]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        stack.append(nei)
+            else:
+                return False
+        return True
