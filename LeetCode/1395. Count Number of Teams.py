cbc5de7c179e685083c58a5105eb52b805d9efbf
James Pak <mingunpa@usc.edu>
2020-04-01 14:58:45 -0700

    greater less solution

--- LeetCode/1395. Count Number of Teams.py
+++ LeetCode/1395. Count Number of Teams.py
@@ -4,10 +4,23 @@ class Solution(object):
         :type rating: List[int]
         :rtype: int
         """
+        if len(rating) < 3:
+            return 0
         res = 0
+        greater = defaultdict(int)
+        less = defaultdict(int)
+        
+        for i in range(len(rating)-1):
+            for j in range(i+1, len(rating)):
+                if rating[j] > rating[i]:
+                    greater[i] += 1
+                elif rating[j] < rating[i]:
+                    less[i] += 1
+        
         for i in range(len(rating)-2):
             for j in range(i+1, len(rating)-1):
-                for k in range(j+1, len(rating)):
-                    if rating[i] > rating[j] > rating[k] or rating[i] < rating[j] < rating[k]:
-                        res += 1
+                if rating[j] > rating[i]:
+                    res += greater[j]
+                elif rating[j] < rating[i]:
+                    res += less[j]
         return res
