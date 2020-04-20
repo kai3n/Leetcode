b36e65dbc0ff4e5b1c5c878ca0c74ec11896f5ba
Mingun Pak <mingunpa@usc.edu>
2020-04-19 17:15:43 -0700

    string solution

--- LeetCode/1419. Minimum Number of Frogs Croaking.py
+++ LeetCode/1419. Minimum Number of Frogs Croaking.py
@@ -1,35 +1,13 @@
-class Solution(object):
+class Solution:
     def minNumberOfFrogs(self, croakOfFrogs):
-        """
-        :type croakOfFrogs: str
-        :rtype: int
-        """
-        c = Counter()
-        m = 0
+        d = {'c': 0, 'r': 0, 'o': 0, 'a': 0, 'k': 0}
+        res = 0
         for letter in croakOfFrogs:
-            if letter == "c":
-                c["c"] += 1
-            elif letter == "r" and c.get("c") >= 1 and c.get("r", 0) - 1 <= c.get("c"):
-                c["r"] += 1
-            elif letter == "o" and c.get("r") >= 1 and c.get("o", 0) - 1 <= c.get("r"):
-                c["o"] += 1
-            elif letter == "a" and c.get("o") >= 1 and c.get("a", 0) - 1 <= c.get("o"):
-                c["a"] += 1
-            elif letter == "k" and c.get("a") >= 1 and c.get("k", 0) - 1 <= c.get("a"):
-                c["c"] -= 1
-                if c["c"] == 0:
-                    del c["c"]
-                c["r"] -= 1
-                if c["r"] == 0:
-                    del c["r"]
-                c["o"] -= 1
-                if c["o"] == 0:
-                    del c["o"]
-                c["a"] -= 1
-                if c["a"] == 0:
-                    del c["a"]
-            else:
+            d[letter] += 1
+            if not (d['c'] >= d['r'] >= d['o'] >= d['a'] >= d['k']):
                 return -1
-            m = max(m, c[letter])
-        return m if len(c) == 0 else -1
+            max_val = max(d['c'], d['r'], d['o'], d['a'], d['k'])
+            min_val = min(d['c'], d['r'], d['o'], d['a'], d['k'])
+            res = max(res, max_val - min_val)
 
+        return res if d['c'] == d['r'] == d['o'] == d['a'] == d['k'] else -1
