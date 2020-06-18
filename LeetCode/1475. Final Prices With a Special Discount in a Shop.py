6c5461c1111747322851a6c82ae34ce538896452
Mingun Pak <mingunpa@usc.edu>
2020-06-17 21:48:10 -0700

    linear solution

--- LeetCode/1475. Final Prices With a Special Discount in a Shop.py
+++ LeetCode/1475. Final Prices With a Special Discount in a Shop.py
@@ -11,3 +11,13 @@ class Solution:
             if not found:
                 res.append(prices[i])
         return res
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        for i, price in enumerate(prices):
+            while stack and prices[stack[-1]] >= price:
+                idx = stack.pop()
+                prices[idx] -= price
+            stack.append(i)
+        return prices
