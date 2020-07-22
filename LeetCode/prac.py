d00c24b11a9d57d4c47479f220f8e77e290968f4
Mingun Pak <mingunpa@usc.edu>
2020-07-21 21:40:55 -0700

    BIT

--- LeetCode/prac.py
+++ LeetCode/prac.py
@@ -1,4 +1,8 @@
-logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
-id_, rest = "dig1 8 1 5 1".split(" ", 1)
-print(id_)
-print()res
+class b():
+    bb = a().aa
+
+class a():
+    aa = 1
+
+
+print(b().bb)
