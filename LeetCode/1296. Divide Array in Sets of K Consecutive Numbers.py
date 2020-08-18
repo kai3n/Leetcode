716524050ad032dbe417ff5d76ed3a8af13fcf34
James Pak <mingunpa@usc.edu>
2020-08-18 16:44:12 +0900

    counter and minus trick

--- LeetCode/1296. Divide Array in Sets of K Consecutive Numbers.py
+++ LeetCode/1296. Divide Array in Sets of K Consecutive Numbers.py
@@ -5,29 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        c = Counter(nums)
-        l = [[key, val] for key, val in c.items()]
-        heapq.heapify(l)
-        while l:
-            i = 0
-            arr = []
-            while i < k:
-                if l:
-                    arr.append(heapq.heappop(l))
-                else:
-                    return False
-                i += 1
-                
-            if len(arr) != k:
-                return False
-                
-            for j in range(1, len(arr)):
-                if arr[j-1][0] + 1 != arr[j][0]:
-                    return False
-                
-            for h in range(len(arr)):
-                if arr[h][1] - 1 != 0:
-                    heapq.heappush(l, [arr[h][0], arr[h][1]-1])
+        count = collections.Counter(nums)
+        keys = sorted(count)
+        for n in keys:
+            if count[n] > 0:
+                minus = count[n]
+                for i in range(n, n + k):
+                    if count[i] < minus:
+                        return False
+                    count[i] -= minus
         return True
-        
         
